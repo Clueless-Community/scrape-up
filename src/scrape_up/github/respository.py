32937f036ccc1c2db1f46fc6515e907fa6535fcf
SARTHAK <shibugarg0303@gmail.com>
2023-02-17 23:55:59 +0530

    languages used scraping

--- src/scrape_up/github/respository.py
+++ src/scrape_up/github/respository.py
@@ -1,6 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
 
 class Repository:
 
     def __init__(self, username: str, repository_name:str):
         self.username = username
         self.repository = repository_name
+
+    def languagesUsed(self):
+        data = requests.get(f"https://github.com/{self.username}/{self.repository}")
+        data = BeautifulSoup(data.text,"html.parser")
+
+        try:
+            languages = data.find_all(class_="color-fg-default text-bold mr-1")
+            allLanguages = []
+            for item in languages:
+                allLanguages.append(item.text)
+            return allLanguages  # return list of languages
+        except:
+            message = "No languages found"
+            return message
