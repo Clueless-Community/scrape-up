b8de7f50f2f0625fab4c24556be9393088c929b4
SARTHAK <shibugarg0303@gmail.com>
2023-02-18 01:43:18 +0530

    private function

--- src/scrape_up/github/respository.py
+++ src/scrape_up/github/respository.py
@@ -8,9 +8,17 @@ class Repository:
         self.username = username
         self.repository = repository_name
 
-    def languagesUsed(self):
+    def __scrape_page(self):
         data = requests.get(f"https://github.com/{self.username}/{self.repository}")
         data = BeautifulSoup(data.text,"html.parser")
+        return data
+
+    def languagesUsed(self):
+
+        """
+        Fetch list of languages used in repository
+        """
+        data = self.__scrape_page()
 
         try:
             languages = data.find_all(class_="color-fg-default text-bold mr-1")
