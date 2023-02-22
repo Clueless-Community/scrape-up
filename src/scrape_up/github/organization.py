371059113f1efd499ef6ee834e78b9b73cf5b9c4
ultron0603@gmail.com <ultron0603@gmail.com>
2023-02-22 22:57:22 +0530

    .top_languages() added in file organizations.py

--- src/scrape_up/github/organization.py
+++ src/scrape_up/github/organization.py
@@ -10,3 +10,16 @@ class Organization:
     def __scrape_page(self):
         data = requests.get(f"https://github.com/{self.organization}")
         data = BeautifulSoup(data.text, "html.parser")
+        return data
+    def top_languages(self):
+        try:
+            languages=[]
+            data=self.__scrape_page()
+            lang_raw=data.find_all("a",class_="no-wrap color-fg-muted d-inline-block Link--muted mt-2")
+            for lang in lang_raw:
+                
+                languages.append(lang.get_text().strip())
+            return languages
+        except:
+            return "An exception occured, cannot get the languages"
+        
