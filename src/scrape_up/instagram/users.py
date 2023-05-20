59bf63bbb7fba7a858ec044db795015db2626d89
Aditya Jethani <aditya.ce21@sot.pdpu.ac.in>
2023-05-21 00:41:27 +0530

    added error handling statements and test for insta

--- src/scrape_up/instagram/users.py
+++ src/scrape_up/instagram/users.py
@@ -1,23 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class Users:
     def __init__(self, username: str):
         self.username = username
 
     def __scrape_page(self):
         username = self.username
-        data = requests.get(f"https://www.instagram.com/{username}/")
-        data = BeautifulSoup(data.text, "html.parser")
-        return data
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+        }
+        response = requests.get(f"https://www.instagram.com/{username}/", headers=headers)
+        if response.status_code == 200:
+            data = BeautifulSoup(response.text, "html.parser")
+            return data
+        else:
+            response.raise_for_status()
 
-    def followers(self) -> str:
+    def followers(self) -> dict:
         """
         Class - `Users`\n
         Example -\n
         ```python
-        user = instagram.User(username="nikhil25803")
+        user = Users(username="nikhil25803")
         followers = user.followers()
         ```
         Return\n
@@ -29,51 +34,21 @@ class Users:
         }
         ```
         """
-        page = self.__scrape_page()
         try:
+            page = self.__scrape_page()
             followers = page.findAll("meta", attrs={"name": "description"})
             followers_count = followers[0]["content"].split(",")[0].split(" ")[0]
             return {
                 "data": followers_count,
                 "message": f"Followers found for user {self.username}",
             }
-        except:
-            message = f"{self.username} not found !"
+        except IndexError:
+            message = f"Failed to retrieve followers count for user {self.username}"
             return {"data": None, "message": message}
-
-    def following(self) -> str:
-        """
-        Class - `Users`\n
-        Example -\n
-        ```python
-        user = instagram.User(username="nikhil25803")
-        following = user.following()
-        ```
-        Return\n
-        ```python
-        return
-        {
-            "data": following_count,
-            "message":f"User {self.username} is following {following_count} people"
-        }
-        ```
-        """
-        page = self.__scrape_page()
-        try:
-            following = page.findAll("meta", attrs={"name": "description"})
-            following_count = (
-                following[0]["content"].split(",")[1].strip().split(" ")[0]
-            )
-            return {
-                "data": following_count,
-                "message": f"User {self.username} is following {following_count} people.",
-            }
-        except:
-            message = f"{self.username} not found !"
+        except requests.exceptions.HTTPError as e:
+            message = f"Failed to retrieve page for user {self.username}. Error: {str(e)}"
             return {"data": None, "message": message}
-
 
 # Test
-# user = Users(username="nikhil_raj803")
-# print(user.followers())
-# print(user.following())
+user = Users(username="nikhil_raj803")
+print(user.followers())
