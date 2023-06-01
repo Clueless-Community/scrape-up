c5d6027e3814f6f019f1beedd93da052e00749bf
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-06-01 12:55:00 +0530

    corrections

--- documentation.md
+++ documentation.md
@@ -207,3 +207,33 @@ following = user.following() #user var taken from above
 print(following)
 ```
 ---
+
+
+## Internshala
+
+```python
+from scrape_up.internshala.internships import Internships
+```
+
+### Scrape Internship details
+
+Create an object of the 'Internships' class:
+
+```python
+scraper = Internships()
+```
+
+| Methods          | Details                                                             |
+| -----------------| --------------------------------------------------------------------|
+| `.internships()` | Scrapes and returns a list of dictionaries representing internships.|
+
+
+**Example:**
+```python
+scraper = Internships()
+internships = scraper.scrape_internships()
+for internship in internships:
+    print(internship)
+
+```
+---
--- src/scrape_up/internshala/internships.py
+++ src/scrape_up/internshala/internships.py
@@ -46,11 +46,13 @@ class Internships:
             internships_container = page.find_all('div', class_='individual_internship')
 
             for internship in internships_container:
-                title = internship.find('div', class_='heading_4_5 profile').text.strip()
-                company = internship.find('div', class_='heading_6 company_name').text.strip()
+                title = internship.find('h3', class_='heading_4_5 profile').text.strip()
+                company = internship.find('h4', class_='heading_6 company_name').text.strip()
                 location = internship.find('a', class_='location_link').text.strip()
-                duration = internship.find('span', class_='internship_durations').text.strip()
-                stipend = internship.find('span', class_='stipend').text.strip()
+                other_details = internship.find_all(class_='item_body')
+                duration = other_details[1].text.strip() if len(other_details) > 2 else "N/A"
+                stipend_element = internship.find('span', class_='stipend')
+                stipend = stipend_element.text.strip() if stipend_element else "N/A"
 
                 internship_data = {
                     'title': title,
@@ -66,6 +68,3 @@ class Internships:
         except Exception as e:
             raise Exception(f"An error occurred while scraping internships: {str(e)}")
 
-
-
- 
