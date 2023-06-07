25c03fd83d324c0835dab4a79aef351e9ced4c8c
Nikhil Raj <nikhil25803@gmail.com>
2023-06-08 00:54:31 +0530

    Courses-> Coursera

--- src/scrape_up/coursera/courses.py
+++ src/scrape_up/coursera/courses.py
@@ -6,18 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 
-class Courses:
+class Coursera:
     def __init__(self, keyword, page_count):
         self.keyword = keyword
         self.page_count = page_count
 
     def __scrape_page(self):
-        chromedriver_path = ''
+        chromedriver_path = ""
         options = Options()
         options.add_argument("--headless")
         driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
         wait = WebDriverWait(driver, 10)
-        driver.get('https://www.coursera.org/search?query=' + self.keyword)
+        driver.get("https://www.coursera.org/search?query=" + self.keyword)
         return wait, driver
 
     def titles(self):
@@ -28,20 +28,27 @@ class Courses:
         titles = []
         try:
             for i in range(self.page_count):
-                courses = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'main ul>li')))
-                titles.extend([driver.execute_script('return arguments[0].querySelector("h3")?.innerText', course) for course in courses])
-                next_btn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next Page"]')
-                if 'disabled' in next_btn.get_attribute('class'):
-                    print('There are no more pages')
+                courses = wait.until(
+                    EC.visibility_of_all_elements_located(
+                        (By.CSS_SELECTOR, "main ul>li")
+                    )
+                )
+                titles.extend(
+                    [
+                        driver.execute_script(
+                            'return arguments[0].querySelector("h3")?.innerText', course
+                        )
+                        for course in courses
+                    ]
+                )
+                next_btn = driver.find_element(
+                    By.CSS_SELECTOR, 'button[aria-label="Next Page"]'
+                )
+                if "disabled" in next_btn.get_attribute("class"):
+                    print("There are no more pages")
                     break
                 else:
                     next_btn.click()
-            return {
-                "data": titles,
-                "message": f"Course Titles for {self.keyword}"
-            }
+            return {"data": titles, "message": f"Course Titles for {self.keyword}"}
         except:
-            return {
-                "data": None,
-                "message": f"No courses found for {self.keyword}"
-            }
+            return {"data": None, "message": f"No courses found for {self.keyword}"}
