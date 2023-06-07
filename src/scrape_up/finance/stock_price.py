255f64b5a980e7179c7dae921085d56c6405306c
Nikhil Raj <nikhil25803@gmail.com>
2023-06-08 01:00:15 +0530

    Stock Price - Check

--- src/scrape_up/finance/stock_price.py
+++ src/scrape_up/finance/stock_price.py
@@ -3,6 +3,7 @@ try:
 except ModuleNotFoundError:
     from scrape_up.finance.nse import NSE
 
+
 class StockPrice:
     """
     This is entry point for users to scrape data.
@@ -18,55 +19,60 @@ class StockPrice:
 
         To get stock price of Infosys from 02-05-2023 to 31-05-2023
         >>> historical_price_info = infosys_stock.get_historical_data('02-05-2023', '31-05-2023')
-            
+
     """
 
-    def __init__(self, stock_name, stock_index='nse'):
+    def __init__(self, stock_name, stock_index="nse"):
         self.stock_name = stock_name.strip()
         self.stock_index = stock_index.strip().lower()
-        if self.stock_index == 'nse':
-            print(f'Searching for {self.stock_name} nse stock...')
+        if self.stock_index == "nse":
+            print(f"Searching for {self.stock_name} nse stock...")
             try:
                 self.stock = NSE(stock_name)
                 self.stock_name = self.stock.stock_name
-                print(f'Found stock based on provided name: {self.stock_name}\nStock instance created')
+                print(
+                    f"Found stock based on provided name: {self.stock_name}\nStock instance created"
+                )
             except:
                 self.stock = None
-                print(f'No stock found based on provided name: {self.stock_name}')
+                print(f"No stock found based on provided name: {self.stock_name}")
         else:
-            print(f'{self.stock_index} stock scraping code under development.\nPlease wait for Update.')
+            print(
+                f"{self.stock_index} stock scraping code under development.\nPlease wait for Update."
+            )
 
     # these two functions could potentially be modified to
     # also return data as pandas format
     def get_latest_price(self):
-        '''
+        """
         Gets Latest stock price info of given stock
-        '''
+        """
         try:
             data = self.stock.get_latest_price()
-            message = f'Found latest stock price for {self.stock_name}'
+            message = f"Found latest stock price for {self.stock_name}"
         except:
             data = None
-            message = f'No latest stock price found for {self.stock_name}'
+            message = f"No latest stock price found for {self.stock_name}"
         finally:
             return {
-                'data' : data,
-                'message' : message,
+                "data": data,
+                "message": message,
             }
 
     def get_historical_data(self, from_date, to_date):
-        '''
+        """
         Gets historical stock price (vwap) in range from_date to to_date
-        '''
+        """
         try:
             data = self.stock.get_historical_data(from_date, to_date)
-            message = f'Historical stock price found for {self.stock_name} in range {from_date} to {to_date}'
+            message = f"Historical stock price found for {self.stock_name} in range {from_date} to {to_date}"
         except:
             data = None
-            message = f'No historial stock price found for {self.stock_name}'
+            message = f"No historial stock price found for {self.stock_name}"
         finally:
             return {
-                'data' : data,
-                'message' : message,
+                "data": data,
+                "message": message,
             }
-
+sp = StockPrice(stock_index="infosys")
+sp.get_historical_data()
