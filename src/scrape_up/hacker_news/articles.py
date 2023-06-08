94e13c50494b9aefb7bccd41d87b1f9119658e29
Patrick Hyatt <patrickhyatt@duck.com>
2023-06-08 00:11:39 +0000

    - Add docstrings detailing HackerNews class/methods

--- src/scrape_up/hacker_news/articles.py
+++ src/scrape_up/hacker_news/articles.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class HackerNews:
+    """
+    Class - `HackerNews` \n
+    Creates a scraper for https://news.ycombinator.com/
+    Example - \n
+    ```python
+    hacker_news = HackerNews()
+    """
     def __init__(self):
         self.help = "This scrapes articles"
 
@@ -12,6 +18,38 @@ class HackerNews:
         return data
 
     def articles_list(self):
+        """
+        Class - `HackerNews` \n
+        Example - \n
+        ```python
+        hacker_news = HackerNews()
+        articles = hacker_news.articles_list()
+        ```
+        Return \n
+        ```python
+        return 
+        {
+            "data": [
+                {
+                    "Article": "Article 1's title/text",
+                    "Link": "https://www.article1.com"
+                },
+                {
+                    "Article": "Article 2's title/text",
+                    "Link": "https://www.article2.com/"
+                },
+                ...
+                {
+                {
+                    "Article": "Article 30's title/text",
+                    "Link": "https://www.article30.com/"
+                }
+                }
+            ],
+            "message": "Successfylly fetched data."
+        }
+        ```
+        """
         page = self.__scrap_page()
         article_list = []
         try:
