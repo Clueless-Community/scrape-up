20d050b67e5a6e2a557a44ce0ce3fdc78435d3ab
Mahima Churi <mahimachuri.28@gmail.com>
2023-06-21 11:59:10 +0530

    [UPDATED] Python script updated

--- src/scrape_up/linkedIn/linkedInspider.py
+++ src/scrape_up/linkedIn/linkedInspider.py
@@ -1,37 +1,33 @@
 import scrapy
 
+
 class LinkedInPeopleProfileSpider(scrapy.Spider):
     name = "linkedin_people_profile"
-
     custom_settings = {
-        'FEEDS': { 'data/%(name)s_%(time)s.jsonl': { 'format': 'jsonlines',}}
-        }
+        'FEEDS': {'data/%(name)s_%(time)s.jsonl': {'format': 'jsonlines',}}
+    }
 
     def start_requests(self):
-        profile_list = ['reidhoffman']   #Any Public Profile can be added
+        profile_list = ['reidhoffman']  # Any public profile username can be added
         for profile in profile_list:
-            linkedin_people_url = f'https://www.linkedin.com/in/{profile}/' 
-            yield scrapy.Request(url=linkedin_people_url, callback=self.parse_profile, meta={'profile': profile, 'linkedin_url': linkedin_people_url})
+            linkedin_people_url = f'https://www.linkedin.com/in/{profile}/'
+            yield scrapy.Request(url=linkedin_people_url, callback=self.parse_profile,
+                                 meta={'profile': profile, 'linkedin_url': linkedin_people_url})
 
     def parse_profile(self, response):
-        item = {}
+        item = LinkedInProfileItem()
         item['profile'] = response.meta['profile']
         item['url'] = response.meta['linkedin_url']
 
-        """
-            SUMMARY SECTION
-        """
         summary_box = response.css("section.top-card-layout")
         item['name'] = summary_box.css("h1::text").get().strip()
         item['description'] = summary_box.css("h2::text").get().strip()
 
-        ## Location
-        try:
-            item['location'] = summary_box.css('div.top-card__subline-item::text').get()
-        except:
-            item['location'] = summary_box.css('span.top-card__subline-item::text').get().strip()
-            if 'followers' in item['location'] or 'connections' in item['location']:
-                item['location'] = ''
+        location_info = summary_box.css('div.top-card__subline-item::text').get()
+        if location_info:
+            item['location'] = location_info.strip()
+        else:
+            item['location'] = ''
 
         item['followers'] = ''
         item['connections'] = ''
@@ -42,29 +38,40 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
             if 'connections' in span_text:
                 item['connections'] = span_text.replace(' connections', '').strip()
 
+        about_section = AboutSection(response)
+        item['about'] = about_section.extract_about()
 
-        """
-            ABOUT SECTION
-        """
-        item['about'] = response.css('section.summary div.core-section-container__content p::text').get(default='')
+        experience_section = ExperienceSection(response)
+        item['experience'] = experience_section.extract_experience()
+
+        education_section = EducationSection(response)
+        item['education'] = education_section.extract_education()
+
+        yield item
 
 
-        """
-            EXPERIENCE SECTION
-        """
-        item['experience'] = []
-        experience_blocks = response.css('li.experience-item')
+class AboutSection:
+    def __init__(self, response):
+        self.response = response
+
+    def extract_about(self):
+        about = self.response.css('section.summary div.core-section-container__content p::text').get(default='')
+        return about
+
+
+class ExperienceSection:
+    def __init__(self, response):
+        self.response = response
+
+    def extract_experience(self):
+        experience_list = []
+        experience_blocks = self.response.css('li.experience-item')
+
         for block in experience_blocks:
             experience = {}
-            ## organisation profile url
             experience['organisation_profile'] = block.css('h4 a::attr(href)').get(default='').split('?')[0]
-                
-                
-            ## location
             experience['location'] = block.css('p.experience-item__location::text').get(default='').strip()
-                
-                
-            ## description
+
             try:
                 experience['description'] = block.css('p.show-more-less-text__text--more::text').get().strip()
             except Exception as e:
@@ -74,8 +81,7 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
                 except Exception as e:
                     print('experience --> description', e)
                     experience['description'] = ''
-                    
-            ## time range
+
             try:
                 date_ranges = block.css('span.date-range time::text').getall()
                 if len(date_ranges) == 2:
@@ -91,41 +97,36 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
                 experience['start_time'] = ''
                 experience['end_time'] = ''
                 experience['duration'] = ''
-            
-            item['experience'].append(experience)
 
-        
-        """
-            EDUCATION SECTION
-        """
-        item['education'] = []
-        education_blocks = response.css('li.education__list-item')
+            experience_list.append(experience)
+
+        return experience_list
+
+
+class EducationSection:
+    def __init__(self, response):
+        self.response = response
+
+    def extract_education(self):
+        education_list = []
+        education_blocks = self.response.css('li.education__list-item')
+
         for block in education_blocks:
             education = {}
-
-            ## organisation
             education['organisation'] = block.css('h3::text').get(default='').strip()
-
-
-            ## organisation profile url
             education['organisation_profile'] = block.css('a::attr(href)').get(default='').split('?')[0]
 
-            ## course details
             try:
-                education['course_details'] = ''
+                course_details = ''
                 for text in block.css('h4 span::text').getall():
-                    education['course_details'] = education['course_details'] + text.strip() + ' '
-                education['course_details'] = education['course_details'].strip()
+                    course_details = course_details + text.strip() + ' '
+                education['course_details'] = course_details.strip()
             except Exception as e:
                 print("education --> course_details", e)
                 education['course_details'] = ''
 
-            ## description
             education['description'] = block.css('div.education__item--details p::text').get(default='').strip()
 
-
-         
-            ## time range
             try:
                 date_ranges = block.css('span.date-range time::text').getall()
                 if len(date_ranges) == 2:
@@ -139,7 +140,20 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
                 education['start_time'] = ''
                 education['end_time'] = ''
 
-            item['education'].append(education)
+            education_list.append(education)
 
-        yield item
-        
+        return education_list
+
+
+class LinkedInProfileItem(scrapy.Item):
+    profile = scrapy.Field()
+    url = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    location = scrapy.Field()
+    followers = scrapy.Field()
+    connections = scrapy.Field()
+    about = scrapy.Field()
+    experience = scrapy.Field()
+    education = scrapy.Field()
+
