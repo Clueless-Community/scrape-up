fbbe8bf9408f96f3d40abebea710f09eddacb27a
Nikhil Raj <nikhil25803@gmail.com>
2023-06-22 18:53:22 +0530

    Update user.py
    Co-Authored-By: Surajdeo Tiwari <58731255+surajdeotiwari@users.noreply.github.com>

--- src/scrape_up/codeforces/user.py
+++ src/scrape_up/codeforces/user.py
@@ -3,20 +3,22 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import json
 
+
 class USER:
     """
     Fetches user data from CodeForces Competitive Platform
     """
+
     user_url = "https://codeforces.com/profile/{}"
 
-    def __init__(self,username):
+    def __init__(self, username):
         self.username = username
         self.userdata = self.user_get_data()
-    
+
     def user_get_data(self):
         """
         Gets all information of a username from codeforces.
-        
+
         Class - `codeforces.user()`\n
         Example -\n
         ```python
@@ -65,21 +67,23 @@ class USER:
             url = self.user_url.format(self.username)
             info = requests.get(url)
             soup = BeautifulSoup(info.content)
-            print(soup.prettify())
-            # d = BeautifulSoup(url)
-            # return 
-            # user_info_set = info.json()
-            # user_info_data = user_info_set['result'][0]
-            # for data in user_info_data:
-            #     print(f'{data} → {user_info_data[data]}')
-            # return "Data Fetching Successful" if user_info_set['status'] == "OK" else "Failed"
+            user_info_set = info.json()
+            user_info_data = user_info_set["result"][0]
+            for data in user_info_data:
+                print(f"{data} → {user_info_data[data]}")
+            return (
+                "Data Fetching Successful"
+                if user_info_set["status"] == "OK"
+                else "Failed"
+            )
         except IndexError:
             raise Exception("Invalid User Name.")
         except TypeError:
             raise Exception("Invalid User Name")
         except requests.exceptions.ConnectionError:
             raise Exception("Connection Error, Please try again.")
+
+
 if __name__ == "__main__":
     user = USER("benq")
     print(user.user_get_data())
-
