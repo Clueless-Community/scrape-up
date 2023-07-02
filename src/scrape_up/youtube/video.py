9d44b28695c287c5cc9f531fc56d6d5145ae3c68
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-07-02 15:38:25 +0530

    Update video.py

--- src/scrape_up/youtube/video.py
+++ src/scrape_up/youtube/video.py
@@ -19,7 +19,26 @@ class Video:
         self.video_url = video_url
 
     def getDetails(self):
-        
+        """
+        Class - `Video`
+        Example:
+        ```
+        video_data = Video("https://www.youtube.com/watch?v=pBy1zgt0XPc")
+        video_data.getDetails()
+        ```
+        Returns:
+        {
+            "title": Tile of the video
+            "description": Description of the video
+            "views_count": No. of views of the video,
+            "upload_date": Upload date of the video,
+            "comment_count": No. of comments,
+            "channel_name": Name of the channel,
+            "channel_avatar": Channel avatar,
+            "subscriber_count": No. of channel subscribers,
+            "channel_url": Link to the channel
+        }
+        """
         url = self.video_url
         try:
             res = requests.get(url)
