cfb9c925cfdd280f75cc4b2ec2dd97dadb231258
Nikhil Raj <nikhil25803@gmail.com>
2023-07-02 14:13:48 +0530

    Made methods private.
    Made the methods private for the time being. We will look into it later if found any alternative.

--- src/scrape_up/spotify/spotify_file.py
+++ src/scrape_up/spotify/spotify_file.py
@@ -2,11 +2,12 @@ import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 class Spotify:
     def __init__(self):
         self.driver = selenium.webdriver.Chrome()
 
-    def scrape_songs_by_keyword(self, keyword):
+    def __scrape_songs_by_keyword(self, keyword):
         """
         Class - `Spotify`\n
         Example -\n
@@ -28,23 +29,36 @@ class Spotify:
             self.driver.get("https://open.spotify.com/search/")
             self.driver.implicitly_wait(100)
 
-            search_box = self.driver.find_element(By.XPATH, "//input[@data-testid='search-input']")
+            search_box = self.driver.find_element(
+                By.XPATH, "//input[@data-testid='search-input']"
+            )
             search_box.send_keys(keyword)
             search_box.send_keys(Keys.RETURN)
 
             songs = []
-            for song in self.driver.find_elements(By.XPATH, "//div[@class='search-results']/section//div[@data-testid='tracklist-row']"):
-                songs.append({
-                    "name": song.find_element(By.XPATH, ".//span[@data-testid='tracklist-row__track-name']").text,
-                    "artist": song.find_element(By.XPATH, ".//span[@data-testid='tracklist-row__artist-name-link']").text,
-                })
+            for song in self.driver.find_elements(
+                By.XPATH,
+                "//div[@class='search-results']/section//div[@data-testid='tracklist-row']",
+            ):
+                songs.append(
+                    {
+                        "name": song.find_element(
+                            By.XPATH,
+                            ".//span[@data-testid='tracklist-row__track-name']",
+                        ).text,
+                        "artist": song.find_element(
+                            By.XPATH,
+                            ".//span[@data-testid='tracklist-row__artist-name-link']",
+                        ).text,
+                    }
+                )
 
             return songs
-        
+
         except Exception as e:
             return None
-    
-    def scrape_homepage(self):
+
+    def __scrape_homepage(self):
         """
         Class - `Spotify`\n
         Example -\n
@@ -67,16 +81,27 @@ class Spotify:
             self.driver.implicitly_wait(100)
 
             playlists = []
-            for song in self.driver.find_elements(By.XPATH, "//div[@data-testid='tracklist-row']"):
-                playlists.append({
-                    "name": song.find_element(By.XPATH, ".//span[@data-testid='tracklist-row__track-name']").text,
-                    "artist": song.find_element(By.XPATH, ".//span[@data-testid='tracklist-row__artist-name-link']").text,
-                })
+            for song in self.driver.find_elements(
+                By.XPATH, "//div[@data-testid='tracklist-row']"
+            ):
+                playlists.append(
+                    {
+                        "name": song.find_element(
+                            By.XPATH,
+                            ".//span[@data-testid='tracklist-row__track-name']",
+                        ).text,
+                        "artist": song.find_element(
+                            By.XPATH,
+                            ".//span[@data-testid='tracklist-row__artist-name-link']",
+                        ).text,
+                    }
+                )
 
             return playlists
-        
+
         except Exception as e:
             return None
-    
+
     def close(self):
         self.driver.quit()
+
