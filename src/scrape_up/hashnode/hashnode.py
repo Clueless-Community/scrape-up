7e610e24e51f07d9f9fd1ff3851c2f0ee3e7db4b
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-07-07 23:33:34 +0530

    added getBlogs() function

---
+++ src/scrape_up/hashnode/hashnode.py
@@ -0,0 +1,81 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+class Hashnode:
+    
+    
+    def getBlogs(self):
+        
+        url = (
+            "https://hashnode.com/community"
+        )
+        try:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+
+            blogs_data = {"blogs": []}
+
+            blogs = soup.find_all("div", class_="css-1s8wn94")
+
+            for b in blogs:
+                title = (
+                    b.find("a", class_="css-4zleql")
+                    .getText()
+                )
+                desc = (
+                    b.find("p", class_="css-1m4ptby")
+                    .getText()
+                    .strip()
+                    .encode("ascii", "ignore")
+                    .decode()
+                )
+                author = (
+                    b.find("a", class_="css-9ssaz8")
+                    .getText()
+                )
+                read_time = (
+                    b.find("span", class_="css-1r5gb7q")
+                    .getText()
+                )
+                try:
+                    like_section = b.find("button", class_="css-a0cwys")
+                    like_count=(
+                        like_section.find("span").getText()
+                    )
+                except:
+                    like_count = "0"
+                try:
+                    comment_section = b.find("a", class_="css-q5e5vl")
+                    comment_count = comment_section.find("span").getText()
+                except:
+                    comment_count = "0"
+                date = (
+                    b.find("a", class_="css-15gyiyx")
+                    .getText()
+                )
+                link = (
+                    b.find("a", class_="css-4zleql", href=True)["href"]
+                )
+
+                blogs_data["blogs"].append(
+                    {
+                        "title": title,
+                        "description": desc,
+                        "author": author,
+                        "read_time": read_time,
+                        "like_count": like_count,
+                        "comment_count": comment_count,
+                        "date": date,
+                        "link": link,
+                    }
+                )
+            res_json = json.dumps(blogs_data)
+            return res_json
+        except:
+            error_message = {
+                "message": "Can't fetch any articles from the topic provided."
+            }
+            ejson = json.dumps(error_message)
+            return ejson
