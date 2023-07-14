5c0f8752d976e3d663156ae9dad421b37c5099b9
Nancy248 <104627064+Nancy248@users.noreply.github.com>
2023-07-14 23:16:40 +0530

    Update main.py
    scarping bcc news by using class and method in python

--- main.py
+++ main.py
@@ -2,33 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Function to scrape BBC News headlines
-def scrape_bbc_news():
-    url = "https://www.bbc.com/news"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = []
+class BBCNewsScraper:
+    def __init__(self):
+        self.base_url = 'https://www.bbc.co.uk'
+        self.headlines_url = self.base_url + '/news'
 
-    # Find all headline elements
-    headline_elements = soup.find_all('h3', class_='gs-c-promo-heading__title')
+    def get_headlines(self):
+        response = requests.get(self.headlines_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        headlines = soup.find_all('h3', class_='gs-c-promo-heading__title')
 
-    # Extract the headlines
-    for headline_element in headline_elements:
-        headline = headline_element.text.strip()
-        headlines.append(headline)
+        news_set = set()
+        news_list = []
+        index = 1
 
-    return headlines
+        for headline in headlines:
+            news_text = headline.get_text(strip=True)
+            if news_text not in news_set:
+                news_set.add(news_text)
+                news_list.append({'index': index, 'headline': news_text})
+                index += 1
+
+        return news_list
 
 
-# Main function
-if __name__ == "__main__":
-    # Scrape headlines
-    news_headlines = scrape_bbc_news()
+# Example usage
+scraper = BBCNewsScraper()
+headlines = scraper.get_headlines()
 
-    # Print headlines and avoid repeated ones with indexing
-    seen_headlines = set()
-    for i, headline in enumerate(news_headlines, start=1):
-        if headline not in seen_headlines:
-            print(f"{i}. {headline}")
-            seen_headlines.add(headline)
+for news in headlines:
+    print(f"{news['index']}. {news['headline']}")
+
+
 
