975d18ab44dd289eb5533282f58811df387d1c1c
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-07-15 15:20:09 +0530

    Update courses.py

--- src/scrape_up/coursera/courses.py
+++ src/scrape_up/coursera/courses.py
@@ -1,63 +1,53 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
 
 
 class Coursera:
-    def __init__(self, keyword, page_count):
-        self.keyword = keyword
-        self.page_count = page_count
+    
+    def __init__(self, topic):
+        self.topic = topic
 
-    def __scrape_page(self):
-        chromedriver_path = ""
-        options = Options()
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
-        wait = WebDriverWait(driver, 10)
-        driver.get("https://www.coursera.org/search?query=" + self.keyword)
-        return wait, driver
-
-    def titles(self):
+    def getCourses(self):
+        
         """
-        Class - `Courses`
-        Example:
-        ```
-        topic = Courses("Machine Learning", 11)
-        courses = topic.titles()
-        ```
-        Returns:
-        {
-            "data": titles,
-            "message": f"Course Titles for {self.keyword}"
-        }
-        """
-        wait, driver = self.__scrape_page()
-        titles = []
+        url = "https://www.coursera.org/search?query=" + self.topic
         try:
-            for i in range(self.page_count):
-                courses = wait.until(
-                    EC.visibility_of_all_elements_located(
-                        (By.CSS_SELECTOR, "main ul>li")
+            res = requests.get(url)
+            soup = BeautifulSoup(res.text, "html.parser")
+
+            courses_data = {"courses": []}
+
+            courses = soup.find_all("div", class_="css-1cj5od")
+
+            for c in courses:
+                try:
+                    title = c.find("h2", class_="cds-119 css-h1jogs cds-121").getText()
+                    teached_by = c.find("span", class_="cds-119 css-1mru19s cds-121").getText()
+                    skills = c.find("p", class_="cds-119 css-12ksubz cds-121").getText()
+                    rating = c.find("p", class_="cds-119 css-11uuo4b cds-121").getText()
+                    review_count = (
+                        c.find("p", class_="cds-119 css-dmxkm1 cds-121")
+                        .getText()
+                        .replace('(','')
+                        .replace(')', '')
                     )
+                    img = c.find("div", class_="css-1doy6bd")
+                    img_url = img.find("img")["src"]
+                    link = c.find("a")["href"]
+                except:
+                    pass
+
+                courses_data["courses"].append(
+                    {
+                        "title": title,
+                        "teached_by": teached_by,
+                        "skills": skills,
+                        "rating": rating,
+                        "review_count": review_count,
+                        "img_url": img_url,
+                        "link": link,
+                    }
                 )
-                titles.extend(
-                    [
-                        driver.execute_script(
-                            'return arguments[0].querySelector("h3")?.innerText', course
-                        )
-                        for course in courses
-                    ]
-                )
-                next_btn = driver.find_element(
-                    By.CSS_SELECTOR, 'button[aria-label="Next Page"]'
-                )
-                if "disabled" in next_btn.get_attribute("class"):
-                    break
-                else:
-                    next_btn.click()
-            return {"data": titles, "message": f"Course Titles for {self.keyword}"}
+            return courses_data
         except:
-            return {"data": None, "message": f"No courses found for {self.keyword}"}
+            return None
