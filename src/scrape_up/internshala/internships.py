44a515974247cf186087d215136637b38d1b9078
Juhi Bhojani <89679280+Juhibhojani@users.noreply.github.com>
2023-07-16 15:19:47 +0530

    Update internships.py
    Fixing error in Internships method

--- src/scrape_up/internshala/internships.py
+++ src/scrape_up/internshala/internships.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 class Internshala:
     """
-    Class to scrape internships from Internshala website.
+    Class to scrape internships and jobs from Internshala website.
     """
 
     def __init__(self, search_type):
@@ -76,95 +76,103 @@ class Internshala:
         - 'stipend': Stipend offered for the internship
         """
         try:
-            url = self.base_url +"internships/"+ self.search_type
+            self.search_type = self.search_type.replace(" ","%20")
+            url = self.base_url+ "internships/" +"keywords-" + self.search_type
             html = self.scrape_page(url)
             page = self.parse_page(html)
             internships = []
 
             internships_container = page.find_all("div", class_="individual_internship")
 
-            for internship in internships_container:
-                title = internship.find("h3", class_="heading_4_5 profile").text.strip()
-                company = internship.find(
-                    "h4", class_="heading_6 company_name"
-                ).text.strip()
-                location = internship.find("a", class_="location_link").text.strip()
-                other_details = internship.find_all(class_="item_body")
-                duration = (
-                    other_details[1].text.strip() if len(other_details) > 2 else "N/A"
-                )
-                stipend_element = internship.find("span", class_="stipend")
-                stipend = stipend_element.text.strip() if stipend_element else "N/A"
+            if not internships_container:
+                return {"message":"No internships found"}
 
-                internship_data = {
-                    "title": title,
-                    "company": company,
-                    "location": location,
-                    "duration": duration,
-                    "stipend": stipend,
+            else:
+                for internship in internships_container:
+                    title = internship.find("h3", class_="heading_4_5 profile").text.strip()
+                    company = internship.find(
+                        "h4", class_="heading_6 company_name"
+                    ).text.strip()
+                    location = internship.find("a", class_="location_link").text.strip()
+                    other_details = internship.find_all(class_="item_body")
+                    duration = (
+                        other_details[1].text.strip() if len(other_details) > 2 else "N/A"
+                    )
+                    stipend_element = internship.find("span", class_="stipend")
+                    stipend = stipend_element.text.strip() if stipend_element else "N/A"
+
+                    internship_data = {
+                        "title": title,
+                        "company": company,
+                        "location": location,
+                        "duration": duration,
+                        "stipend": stipend,
+                    }
+
+                    internships.append(internship_data)
+
+                return {
+                    "data": internships,
+                    "message": "Internships are now fetched",
                 }
-
-                internships.append(internship_data)
-
-            return {
-                "data": internships,
-                "message": "Internships are now fetched",
-            }
         except Exception as e:
             raise Exception(f"An error occurred while scraping internships: {str(e)}")
 
+
     def jobs(self):
-    """
-    Fetches the jobs data.
+        """
+        Fetches the jobs data.
 
-    Class - `Jobs`
-    Example:
-    ```
-    scraper = Internshala(search_type="keyword")
-    jobs = scraper.jobs()
-    ```
+        Class - `Jobs`
+        Example:
+        ```
+        scraper = Internshala(search_type="keyword")
+        jobs = scraper.jobs()
+        ```
 
-    Returns:
-    List of dictionaries, where each dictionary represents a job and contains the following keys:
-    - 'title': Title of the job
-    - 'company': Company offering the job
-    - 'location': Location of the job
-    - 'CTC': CTC (Compensation) offered for the job
-    - 'experience(in years)': Required experience for the job
-    """
-    try:
-        self.search_type = self.search_type.replace(" ", "%20")
-        url = self.base_url + "jobs/" + "keywords-" + self.search_type
-        html = self.scrape_page(url)
-        page = self.parse_page(html)
-        jobs = []
+        Returns:
+        List of dictionaries, where each dictionary represents a job and contains the following keys:
+        - 'title': Title of the job
+        - 'company': Company offering the job
+        - 'location': Location of the job
+        - 'CTC': CTC (Compensation) offered for the job
+        - 'experience(in years)': Required experience for the job
+        """
+        try:
+            self.search_type = self.search_type.replace(" ", "%20")
+            url = self.base_url + "jobs/" + "keywords-" + self.search_type
+            print(url)
+            html = self.scrape_page(url)
+            page = self.parse_page(html)
+            jobs = []
 
-        jobs_container = page.find("div", {"id": "internship_list_container_1"})
+            jobs_container = page.find("div", {"id": "internship_list_container_1"})
 
-        if not jobs_container.text:
-            return {"message":"No jobs found"}
-        else:
-            for item in jobs_container.find_all("div", {"class": "container-fluid individual_internship visibilityTrackerItem"}):
-                title = item.find("h3", {"class": "heading_4_5 profile"}).text.strip()
-                company = item.find("h4", {"class": "heading_6 company_name"}).text.strip()
-                location = item.find("p", {"id": "location_names"}).text.strip()
-                ctc = item.find("div", {"class": "item_body salary"}).text.strip()
-                experience = item.find("div", {"class": "item_body desktop-text"}).text.strip().split()[0]
+            if not jobs_container.text:
+                return {"message":"No jobs found"}
+            else:
+                for item in jobs_container.find_all("div", {"class": "container-fluid individual_internship visibilityTrackerItem"}):
+                    title = item.find("h3", {"class": "heading_4_5 profile"}).text.strip()
+                    company = item.find("h4", {"class": "heading_6 company_name"}).text.strip()
+                    location = item.find("p", {"id": "location_names"}).text.strip()
+                    ctc = item.find("div", {"class": "item_body salary"}).text.strip()
+                    experience = item.find("div", {"class": "item_body desktop-text"}).text.strip().split()[0]
 
-                job_data = {
-                    "title": title,
-                    "company": company,
-                    "location": location,
-                    "CTC": ctc,
-                    "experience(in years)": experience,
+                    job_data = {
+                        "title": title,
+                        "company": company,
+                        "location": location,
+                        "CTC": ctc,
+                        "experience(in years)": experience,
+                    }
+
+                    jobs.append(job_data)
+
+                return {
+                    "data": jobs,
+                    "message": "Jobs are now fetched",
                 }
+        except Exception as e:
+            raise Exception(f"An error occurred while scraping jobs: {str(e)}")
 
-                jobs.append(job_data)
-
-            return {
-                "data": jobs,
-                "message": "Jobs are now fetched",
-            }
-    except Exception as e:
-        raise Exception(f"An error occurred while scraping jobs: {str(e)}")
 
