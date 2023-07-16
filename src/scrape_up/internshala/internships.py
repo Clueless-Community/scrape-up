f33fbc56de5085ef3a7885af631abd3980744af9
Juhi Bhojani <89679280+Juhibhojani@users.noreply.github.com>
2023-07-16 15:16:42 +0530

    Update internships.py
    Adding jobs method in Class internshala

--- src/scrape_up/internshala/internships.py
+++ src/scrape_up/internshala/internships.py
@@ -15,7 +15,7 @@ class Internshala:
 
 class Internshala:
     def __init__(self, search_type):
-        self.base_url = "https://internshala.com/internships/"
+        self.base_url = "https://internshala.com/"
         self.search_type = search_type
 
     def scrape_page(self, url):
@@ -76,7 +76,7 @@ class Internshala:
         - 'stipend': Stipend offered for the internship
         """
         try:
-            url = self.base_url + self.search_type
+            url = self.base_url +"internships/"+ self.search_type
             html = self.scrape_page(url)
             page = self.parse_page(html)
             internships = []
@@ -112,3 +112,59 @@ class Internshala:
             }
         except Exception as e:
             raise Exception(f"An error occurred while scraping internships: {str(e)}")
+
+    def jobs(self):
+    """
+    Fetches the jobs data.
+
+    Class - `Jobs`
+    Example:
+    ```
+    scraper = Internshala(search_type="keyword")
+    jobs = scraper.jobs()
+    ```
+
+    Returns:
+    List of dictionaries, where each dictionary represents a job and contains the following keys:
+    - 'title': Title of the job
+    - 'company': Company offering the job
+    - 'location': Location of the job
+    - 'CTC': CTC (Compensation) offered for the job
+    - 'experience(in years)': Required experience for the job
+    """
+    try:
+        self.search_type = self.search_type.replace(" ", "%20")
+        url = self.base_url + "jobs/" + "keywords-" + self.search_type
+        html = self.scrape_page(url)
+        page = self.parse_page(html)
+        jobs = []
+
+        jobs_container = page.find("div", {"id": "internship_list_container_1"})
+
+        if not jobs_container.text:
+            return {"message":"No jobs found"}
+        else:
+            for item in jobs_container.find_all("div", {"class": "container-fluid individual_internship visibilityTrackerItem"}):
+                title = item.find("h3", {"class": "heading_4_5 profile"}).text.strip()
+                company = item.find("h4", {"class": "heading_6 company_name"}).text.strip()
+                location = item.find("p", {"id": "location_names"}).text.strip()
+                ctc = item.find("div", {"class": "item_body salary"}).text.strip()
+                experience = item.find("div", {"class": "item_body desktop-text"}).text.strip().split()[0]
+
+                job_data = {
+                    "title": title,
+                    "company": company,
+                    "location": location,
+                    "CTC": ctc,
+                    "experience(in years)": experience,
+                }
+
+                jobs.append(job_data)
+
+            return {
+                "data": jobs,
+                "message": "Jobs are now fetched",
+            }
+    except Exception as e:
+        raise Exception(f"An error occurred while scraping jobs: {str(e)}")
+
