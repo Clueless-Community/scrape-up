009c19f0aa17c26402508e98d8ccc2483a9b7aae
Nikhil Raj <nikhil25803@gmail.com>
2023-07-19 23:15:58 +0530

    Code Refactor
    Co-Authored-By: Jaivardhan Shukla <93859359+jaivsh@users.noreply.github.com>

--- src/scrape_up/MediEncyclopedia/mediencyclopedia.py
+++ src/scrape_up/MediEncyclopedia/mediencyclopedia.py
@@ -1,32 +1,25 @@
-from html.parser import HTMLParser
-import bs4
 from bs4 import BeautifulSoup
 import requests
 
-"""
-        Create an instance of `MediEncyclopedia` class.
 
+class MediEncyclopedia:
+    """
+    Create an instance of `MediEncyclopedia` class.
     ```python
     ency = MediEncyclopedia()
     ```
-
     | Methods               | Details                                                                                      |
     | --------------------- | -------------------------------------------------------------------------------------------- |
     | `.scrapebyurl()`      | Returns the medical dictation of associated topic url                                        |
     | `.query()`            | It takes a user query parameter as an argument and returns all relevant terms related to it. |
     | `.byletter()`         | Returns the list of medical relics starting with a particular letter                         |
-
-
     """
 
+    def __init__(self):
+      pass
 
-class MediEncyclopedia:
-
-  def __init__(self):
-    print(self)
-
-  def scrapebyurl(self,url):
-    """
+    def scrapebyurl(self, url):
+        """
         Class - `MediEncyclopedia`
         Example:
         ```
@@ -34,92 +27,98 @@ class MediEncyclopedia:
         ency.scrapebyurl('url goes here')
         ```
         Returns:
+        ```js
         {
             "headline": The name of the encyclopedia entry
             "text": Link to the entry
         }
-    """      
-   
-    try:
-      content = requests.get(url)
-      soup = BeautifulSoup(content.content, 'html.parser')
-      headline = soup.find('h1',attrs={'class':'with-also'}).text
-      article = soup.find('article')
-      for script in article(["script", "style"]):
-        script.extract()
-      text = article.get_text().replace('Browse the Encyclopedia','').replace('Clinical Trials','')
-      text = text.replace('To use the sharing features on this page, please enable JavaScript.','')
+        ```
+        """
 
-      return [headline,text]
-    except:
-      return None
+        try:
+            content = requests.get(url)
+            soup = BeautifulSoup(content.content, "html.parser")
+            headline = soup.find("h1", attrs={"class": "with-also"}).text
+            article = soup.find("article")
+            for script in article(["script", "style"]):
+                script.extract()
+            text = (
+                article.get_text()
+                .replace("Browse the Encyclopedia", "")
+                .replace("Clinical Trials", "")
+            )
+            text = text.replace(
+                "To use the sharing features on this page, please enable JavaScript.",
+                "",
+            )
 
-  def query(self, userquery):
-    """
+            return [headline, text]
+        except:
+            return None
+
+    def query(self, userquery):
+        """
         Class - `MediEncyclopedia`
         Example:
         ```
         ency = MediEncyclopedia()
-        ency.query('query goes here')
+        ency.query(userquery='query goes here')
         ```
         Returns:
+        ```js
         {
             "headline": The name of the encyclopedia entry
             "text": Link to the entry
         }
-    """   
-    try:
+        ```
+        """
+        try:
+            fl = userquery[0].upper()
+            resq = []
+            vals = MediEncyclopedia().byletter(fl)
+            ls = userquery.split(" ")
+            for i in ls:
+                for j in vals:
+                    if i in j[0]:
+                        resq.append(j)
+            resq = list(resq)
+            return resq
+        except:
+            return None
 
-      fl = userquery[0].upper()
-      resq = []
-      vals = MediEncyclopedia().byletter(fl)
-      ls = userquery.split(' ')
-      for i in ls:
-        for j in vals:
-          if i in j[0]:
-            resq.append(j)
-      resq = list(resq)
-      return resq
-    except:
-      return None      
-
-
-
-
-
-  def byletter(self, character):
-    """
+    def byletter(self, character):
+        """
         Class - `MediEncyclopedia`
         Example:
         ```
         ency = MediEncyclopedia()
-        ency.byletter('single english character goes here')
+        ency.byletter(character='single english character goes here')
         ```
         Returns:
+        ```js
         {
             "headline": The name of the encyclopedia entry
             "text": Link to the entry
         }
-    """
-    try:
-      character = character.upper()
-      chk = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      if character not in chk:
-        return None
-      url = 'https://medlineplus.gov/ency/encyclopedia_{}.htm'.format(character)
-      content = requests.get(url)
-      base = 'https://medlineplus.gov/ency/'
-      soup = BeautifulSoup(content.content, 'html.parser')
-      vals = []
-      heads = soup.find_all('li')
-      allheads = heads[42:len(heads)-16]
-      for i in allheads:
-        t = base+i.find('a')['href']
-        h = i.find('a').text
-        vals.append([h,t])
+        ```
+        """
+        try:
+            character = character.upper()
+            chk = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            if character not in chk:
+                return None
+            url = "https://medlineplus.gov/ency/encyclopedia_{}.htm".format(character)
+            content = requests.get(url)
+            base = "https://medlineplus.gov/ency/"
+            soup = BeautifulSoup(content.content, "html.parser")
+            vals = []
+            heads = soup.find_all("li")
+            allheads = heads[42 : len(heads) - 16]
+            for i in allheads:
+                t = base + i.find("a")["href"]
+                h = i.find("a").text
+                vals.append([h, t])
 
-      return vals
-    except:
-      return None
-
-
+            return vals
+        except:
+            return None
