4406714ce5b0dc83f81b794431df80026550c582
Swapnil Singh <swapnilskumars@gmail.com>
2023-07-26 03:53:34 +0530

    added a get_article method in BBCNews

--- src/scrape_up/BBCNews/bbc_news.py
+++ src/scrape_up/BBCNews/bbc_news.py
@@ -29,7 +29,7 @@ class BBCNews:
         response = requests.get(self.headlines_url)
         soup = BeautifulSoup(response.content, "html.parser")
         headlines = soup.find_all("h3", class_="gs-c-promo-heading__title")
-
+        print(headlines[0])
         news_set = set()
         news_list = []
         index = 1
@@ -42,3 +42,34 @@ class BBCNews:
                 index += 1
 
         return news_list
+    
+    def get_article(self,url):
+        """
+        Create an instance of the class - `BBCNews`\n
+        ```python
+        scraper = BBCNews()
+        article = scraper.get_article()
+        print(article)
+        ```
+        """
+        try:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+            }
+            response = requests.get(url, headers=headers).text
+            soup = BeautifulSoup(response, "lxml")
+
+            main_heading = soup.find("h1", {"id": "main-heading"}).text.strip()
+            time = soup.find("time").text.strip()
+            text_content = soup.find_all("div", {"data-component": "text-block"})
+            Text = ""
+            for text in text_content:
+                Text += text.text.strip()+" "
+            Data = {
+                "main_heading": main_heading,
+                "time": time,
+                "text": Text
+            }
+            return Data
+        except:
+            return None
