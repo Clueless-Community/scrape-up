1d3ee05d12132fc324c79edeeec22ad80173f9bb
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-07-26 22:30:49 +0530

    updated docstring of get_feed() function

--- src/scrape_up/hashnode/hashnode.py
+++ src/scrape_up/hashnode/hashnode.py
@@ -16,7 +16,26 @@ class Hashnode:
     """
 
     def get_feed(self):
-        
+        """
+        Class - `Hashnode`
+        Example:
+        ```pyton
+        blogs = Hashnode()
+        blogs.get_feed()
+        ```
+        Returns:
+        ```js
+        {
+            "title": Title of the blog
+            "description": Description of the blog
+            "author": Author of the blog
+            "like_count": No. of likes of the blog
+            "comment_count": No. of comments of the blog
+            "date": Date the blog was posted
+            "link": Link to the blog
+        }
+        ```
+        """
         url = "https://hashnode.com/community"
         try:
             res = requests.get(url)
