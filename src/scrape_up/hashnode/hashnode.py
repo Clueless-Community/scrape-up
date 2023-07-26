adbff788055298cccb25f9f7fc1f31395e16b157
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-07-26 23:05:22 +0530

    updated docstring in Hashnode class

--- src/scrape_up/hashnode/hashnode.py
+++ src/scrape_up/hashnode/hashnode.py
@@ -13,6 +13,7 @@ class Hashnode:
     | `.get_feed()`     | Returns the blogs with title, descriptions, author, read time, like and comment count, date and link |
     | `.get_featured()` | Returns the featured blogs with title, descriptions, author, like and comment count, date and link   |
     | `.get_recent()`   | Returns the recent blogs with title, descriptions, author, like and comment count, date and link     |
+    | `.search(topic)`  | Returns the blogs with title, descriptions, author, like and comment count, date and link for a topic|
     """
 
     def get_feed(self):
