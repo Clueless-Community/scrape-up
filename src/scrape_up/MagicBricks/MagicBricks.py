8a97ada770f5deaa08743b5f2c125d6ff2269d15
Juhi Bhojani <89679280+Juhibhojani@users.noreply.github.com>
2023-08-01 23:53:48 +0530

    Add files via upload

---
+++ src/scrape_up/MagicBricks/MagicBricks.py
@@ -0,0 +1,98 @@
+from bs4 import BeautifulSoup
+import requests
+
+class MagicBricks:
+    """
+    Create an instance of the `MagicBricks` class to fetch ready-to-move flats data.
+
+    :Example:
+    ```python
+    magic_bricks = MagicBricks()
+    flats_data = magic_bricks.get_ready_to_move(city="Agra")
+    print(flats_data)
+    ```
+
+    | Method                           | Details                                                      |
+    | -------------------------------- | ------------------------------------------------------------ |
+    | `get_ready_to_move(city)`        | Fetches and returns the details of ready-to-move flats in the specified city.
+
+    """
+    def __init__(self):
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"}
+
+    def get_ready_to_move(self,city):
+        """
+        Fetches and returns the details of ready-to-move flats in the specified city.
+
+        :param city: The name of the city (e.g., "Agra").
+        :type city: str
+        :return: A list of dictionaries containing details of ready-to-move flats.
+        :rtype: list
+
+        Each dictionary in the list contains the following information:
+        - "Title": The title of the flat listing.
+        - "Details": A list of dictionaries containing details like number of bedrooms, bathrooms, area, etc.
+        - "Description": A brief description of the flat.
+        - "Amount": The total price of the flat.
+        - "Price Per SQFT": The price per square foot of the flat (if available, otherwise None).
+
+        Example output:
+        ```python
+        [
+            {
+                "Title": "2 BHK Apartment in Agra",
+                "Details": [
+                    {"Beds": "2 BHK"},
+                    {"Baths": "2 Baths"},
+                    {"Area": "900 sqft"},
+                    {"Furnished": "Unfurnished"},
+                    {"Status": "Ready to Move"},
+                    {"Floor": "3"},
+                ],
+                "Description": "Apartment Description...",
+                "Amount": "₹ 45 Lac",
+                "Price Per SQFT": "₹ 5,000/sqft"
+            },
+            ...
+        ]
+        ```
+        """
+        try:
+            city = city.lower()
+            url = f"https://www.magicbricks.com/ready-to-move-flats-in-{city}-pppfs"
+            html_text = requests.get(url, headers=self.headers).text
+            soup = BeautifulSoup(html_text, "lxml")
+
+            houses = []
+            container = soup.find("div",{"class":"mb-srp__left"})
+            for items in container.find_all("div",{"class":"mb-srp__list"}):
+                title = items.find("h2")
+                i = 0
+                labels = []
+                for item in items.find_all("div",{"class":"mb-srp__card__summary__list--item"}):
+                    if i<6:
+                        label = item.find("div",{"class":"mb-srp__card__summary--label"})
+                        value = item.find("div",{"class":"mb-srp__card__summary--value"})
+                        labels.append({label.text:value.text})
+                        i+=1
+                    else:
+                        break
+                description = items.find("div",{"class":"mb-srp__card--desc--text"})
+                amount = items.find("div",{"class":"mb-srp__card__price--amount"})
+                price_per_sqft = items.find("div",{"class":"mb-srp__card__price--size"})
+                if price_per_sqft:
+                    price_per_sqft = price_per_sqft.text
+                else:
+                    price_per_sqft = None
+                data = {
+                    "Title":title.text,
+                    "Details":labels,
+                    "Description":description.text,
+                    "Amount":amount.text,
+                    "Price Per SQFT":price_per_sqft
+                }
+                houses.append(data)
+            return houses
+        except:
+            return None
+
