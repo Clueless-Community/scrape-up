da754e4bee9f0e7d91b91fc7561f664a5dcc0191
Simran <simran28singh28@gmail.com>
2023-08-09 00:28:51 +0530

    Refactor Exception Handling and Method Documentation

--- src/scrape_up/Academia/academia_scraper.py
+++ src/scrape_up/Academia/academia_scraper.py
@@ -3,15 +3,46 @@ import requests
 
 class Academia:
     """
-    ... (class documentation) ...
+    Create an instance of `Academia` class
+
+    ```python
+    academia = Academia()
+    ```
+
+    | Method                        | Details                                                               |
+    | ----------------------------- | --------------------------------------------------------------------- |
+    | `get_research_topics(letter)` | Fetches and returns research topics starting with the given letter.   |
+    | `get_research_papers(search)` | Fetches and returns research papers related to the given search term. |
+
     """
 
     def __init__(self):
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36"
         }
 
     def get_research_topics(self, topic="None"):
+        """
+        Fetches and returns research topics starting with the given letter.\n
+        Param `letter`: The letter to filter research topics (default is "None" to get all topics).\n
+        Class - `Academia`\n
+        ```python
+        ac = Academia()
+        ac.get_research_topics(topic="machine learning")
+        ```
+        Example output:
+        ```python
+        [
+            {
+                "Title": "Artificial Intelligence",
+                "Link": "https://www.academia.edu/topics/artificial_intelligence",
+                "Number of Articles": "20,000",
+                "Followers": "5,000"
+            },
+            ...
+        ]
+        ```
+        """
         try:
             letter = topic.capitalize()
             url = f"https://www.academia.edu/topics/{letter}"
@@ -22,19 +53,19 @@ class Academia:
             container = soup.find("div", {"class": "topic-list-container"})
 
             if container:
-            for item in container.find_all("div", {"class": "topic-list-item"}):
-                link = item.find("a", href=True)["href"]
-                title = item.find("a").text
-                articles = item.find("p")
-                followers = articles.next_sibling.next_sibling  
+                for item in container.find_all("div", {"class": "topic-list-item"}):
+                    link = item.find("a", href=True)["href"]
+                    title = item.find("a").text
+                    articles = item.find("p")
+                    followers = articles.find_next_sibling("p")  # Use find_next_sibling to get the followers
 
-                data = {
-                    "Title": title,
-                    "Link": link,
-                    "Number of Articles": articles.text if articles else "N/A",
-                    "Followers": followers.text if followers else "N/A",
-                }
-                topics.append(data)
+                    data = {
+                        "Title": title,
+                        "Link": link,
+                        "Number of Articles": articles.text if articles else "N/A",
+                        "Followers": followers.text if followers else "N/A",
+                    }
+                    topics.append(data)
             else:
                 print("Container not found")
                 return None
@@ -44,7 +75,7 @@ class Academia:
         except Exception as e:
             print(f"An error occurred: {e}")
 
-        return None
+        return topics
 
     def get_research_papers(self, search):
         """
@@ -76,14 +107,14 @@ class Academia:
 
             papers = []
             container = soup.find("div", {"class": "works"})
-            for items in container:
-                title = items.find("div", {"class": "header"}).text
-                summary = items.find("div", {"class": "complete hidden"})
+            for item in container.find_all("div", {"class": "item"}):  # Loop through items
+                title = item.find("div", {"class": "header"}).text
+                summary = item.find("div", {"class": "description"})
                 if summary:
                     summary = summary.text
                 else:
                     summary = None
-                link = items.find("a", href=True)["href"]
+                link = item.find("a", href=True)["href"]
                 data = {"Title": title, "Summary": summary, "Link": link}
                 papers.append(data)
             return papers
