b571a3392e9069682ec502d25bcd8e87f11582ca
Ritik Raj <raj769417@gmail.com>
2023-08-10 00:25:27 +0530

    added method to get all hackathons

--- src/scrape_up/devpost/devpost.py
+++ src/scrape_up/devpost/devpost.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import undetected_chromedriver as uc
+import re
+import json
 
 class Devpost:
     """
@@ -11,6 +15,7 @@ class Devpost:
     | Methods            | Details                                                                                                              |
     | ------------------ | -------------------------------------------------------------------------------------------------------------------- |
     | `.get_projects()` | Returns the latest projects along with their decription, like and commment count, image and member details. |
+    | `.get_hackathons()` | Returns the latest hackathons along with their title, participants, prizes, deadlines.          |
     """
 
     def get_projects(self):
@@ -81,3 +86,84 @@ class Devpost:
             return projects_data["projects"]
         except:
             return None
+
+    def get_hackathons(self):
+        """
+        Class - `Devpost`
+        Example -
+        ```python
+        devpost = Devpost()
+        hackathons = devpost.hackathons()
+        ```
+        Return
+        ```js
+        [
+            {
+                'title': 'Ripple CBDC Innovate',
+                'status': '9 days left',
+                'prize': '200,000',
+                'total participants': '1061',
+                'host': 'Ripple',
+                'submission-period': 'May 15 - Aug 18, 2023',
+                'labels': 'Blockchain, Fintech, Social Good, ',
+                'hackathon-image': 'https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/459/630/datas/medium_square.png'
+            }
+            ...
+        ]
+        ```
+        """
+
+        url = "https://devpost.com/hackathons"
+
+        try:
+            chrome_options = uc.ChromeOptions()
+            chrome_options.add_argument("--headless")
+            driver = uc.Chrome(options=chrome_options)
+
+            driver.get(url)
+            wait = WebDriverWait(driver, 5)
+        except:
+            return None
+
+        head_tags = wait.until(
+            lambda d: driver.find_elements(By.CLASS_NAME, 'hackathon-tile')
+        )
+        hackathons_data = {"hackathons": []}
+        try:
+            for tag in head_tags:
+                main_content = tag.find_element(By.CLASS_NAME, 'main-content')
+                side_content = tag.find_element(By.CLASS_NAME, 'side-info')
+
+                img = main_content.find_element(By.TAG_NAME, 'img').get_attribute('src')
+
+                title = main_content.find_element(By.CSS_SELECTOR, '.content h3').text
+
+                status = main_content.find_element(By.CLASS_NAME, 'hackathon-status').text
+
+                prize = main_content.find_element(By.CSS_SELECTOR, '.prize-amount span').text
+
+                participants = main_content.find_element(By.CSS_SELECTOR, '.participants strong').text
+
+                host = side_content.find_element(By.CLASS_NAME, 'host-label').text
+                submission = side_content.find_element(By.CLASS_NAME, 'submission-period').text
+
+                theme_labels = side_content.find_elements(By.CLASS_NAME, 'theme-label')
+                theme = ''
+                for label in theme_labels:
+                    theme = theme + label.text + ', '
+
+                hackathon = {
+                    'title': title,
+                    'status': status,
+                    'prize': prize,
+                    'total participants': participants,
+                    'host': host,
+                    'submission-period': submission,
+                    'labels': theme,
+                    'hackathon-image': img
+                }
+
+                hackathons_data["hackathons"].append(hackathon)
+        except:
+            return None
+        return hackathons_data
