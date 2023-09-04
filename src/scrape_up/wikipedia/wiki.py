6ccbc96e66447018f7e17f21f82462b2a4711f7a
Mehdi Habibi <106245122+metflekx@users.noreply.github.com>
2023-09-04 21:21:06 +0000

    define method with tests, at wiki.py

--- src/scrape_up/wikipedia/wiki.py
+++ src/scrape_up/wikipedia/wiki.py
@@ -3,7 +3,7 @@ import requests
 import re
 
 
-class WikipediaScraper:
+class Wiki:
     """
     Create an object of the 'WikipediaScrapper' class:
 
@@ -43,32 +43,59 @@ class WikipediaScraper:
         Returns the definition of the query from Wikipedia
         """
 
-        div = self.soup.find(id="mw-content-text")
-
-        # get the first p that is not empty
-        paragraphs = div.find_all("p")
-        for p in paragraphs:
-            if p.text and p.text != "\n":
-                # remove the reference numbers in square brackets and numbers whitin square brackets
-                definition = p.text
-                definition = re.sub(r'\[[0-9]*\]', '', definition)
-        
-                definition = definition.split(".")[0]
-                
-                return definition
-
         try:
-            # get first p
-            definition = self.soup.find("p").text
+            div = self.soup.find(id="mw-content-text")
 
-            return definition
+            # get the first p that is not empty
+            paragraphs = div.find_all("p")
+            for p in paragraphs:
+                if p.text and p.text != "\n":
+                    # remove the reference numbers in square brackets and numbers whitin square brackets
+                    definition = p.text
+                    definition = re.sub(r'\[[0-9]*\]', '', definition)
+        
+                    definition = definition.split(".")[0]
 
+                    return definition
+
+        
+                # get first paragraph
+                definition = self.soup.find("p").text
+
+                return definition
         except Exception as e:
-            print(f"Error: {e}")
+            print(f".define() failed : {e}")
             return None
 
 if __name__ == "__main__":
-    dog_wiki = WikipediaScraper("dog")
-    dog_wiki = WikipediaScraper("graph theory")
+    # test
+    import os
+    import time
+    
+    test_queries = ["dog", "graph theory", "stanford", "computer science", 
+                    "machine learning", "artificial intelligence", "data science",
+                     "data structure", "programming language", "computer", "computer network", 
+                     "computer security", "computer graphics", "computer vision", 
+                     "computer architecture", "computer engineering", "computer hardware",
+                       "computer software", "computer programming", "computer data storage", 
+                       "computer memory", "computer performance", "computer science", "computer scientist",
+                         "computer programming", "computer programmer", "computer keyboard", "computer mouse",
+                           "computer monitor", "computer terminal", "computer network", "computer security", 
+                           "computer graphics", "computer vision", "computer architecture", "computer engineering", 
+                           "computer hardware", "computer software", "computer programming", "computer data storage",
+                             "computer memory", "computer performance", "computer science", "computer scientist", 
+                             "computer programming", "computer programmer", "computer keyboard", "computer mouse", "computer monitor", "computer terminal"]
 
-    print(dog_wiki.define())
+    for query in test_queries:
+        if os.name == "posix":
+            os.system("clear")
+        if os.name == "nt":
+            os.system("cls")
+
+        wiki = Wiki(query)
+        print(wiki.define())
+        time.sleep(4)
+        # The dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf
+        # In mathematics, graph theory is the study of graphs, which are mathematical structures used to model pairwise relations between objects
+        # Stanford University (officially Leland Stanford Junior University) is a private research university in Stanford, California
+        # . . . 
