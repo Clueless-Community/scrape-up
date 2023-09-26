a36d78b86bf8ffe7dde4fe9c08c9792e7d980dcf
Mehdi <106245122+metflekx@users.noreply.github.com>
2023-09-26 06:31:44 +0300

    added changes to already existing wikipedia.py

--- src/scrape_up/wikipedia/wikipedia.py
+++ src/scrape_up/wikipedia/wikipedia.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 
-class WikipediaScraper:
+class Wiki:
     """
     Create an object of the 'WikipediaScrapper' class:
 
@@ -10,14 +11,61 @@ class WikipediaScraper:
     Scraper = WikipediaScraper()
     ```
 
-    | Methods           | Details                                                 |
-    | ----------------- | ------------------------------------------------------- |
-    | `.scrape(url)`    | Returns the Scraped Data from Wikipedia                 |
-    | `.get_featured()` | Returns the featured article for the day from Wikipedia |
+    | Methods            | Details                                                                          |
+    | -----------------  | ---------------------------------------------------------------------------------|
+    | `.scrape(url)`     | Returns the Scraped Data from Wikipedia                                          |
+    | `.get_featured()`  | Returns the featured article for the day from Wikipedia                          |
+    | `.__scrape(query)` | private method to scrape the data from wikipedia, Returns a BeautifulSoup object |
+    | `.define()`        | Returns the definition of the query from Wikipedia as string                     |
+
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, query):
+        self.query = query
+
+    def __scrape(self) -> BeautifulSoup:
+        """ 
+        private method to scrape the data from wikipedia, returns a BeautifulSoup object
+        """
+
+        URL = f"https://en.wikipedia.org/wiki/{self.query}"
+
+        try:
+            response = requests.get(URL)
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            return soup
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
+
+    def define(self):
+        """
+        Uses regex and BeautifulSoup to get the definition of the query from Wikipedia 
+        Returns the definition of the query from Wikipedia, as string
+        """
+
+        soup = self.__scrape()
+
+        try:
+            div = soup.find(id="mw-content-text")
+
+            # get the first p that is not empty
+            paragraphs = div.find_all("p")
+            for p in paragraphs:
+                if p.text and p.text != "\n":
+                    # remove the reference numbers in square brackets and numbers whitin square brackets
+                    definition = p.text
+                    definition = re.sub(r'\[[0-9]*\]', '', definition)
+        
+                    definition = definition.split(".")[0]
+
+                    return definition
+                    
+        except Exception as e:
+            print(f".define() failed : {e}")
+            return None
 
     def scrape(self, query):
         try:
@@ -61,6 +109,7 @@ class WikipediaScraper:
         Returns:
         A string containing the featured data from the main page of Wikipedia.
         """
+
         try:
             url = "https://en.wikipedia.org/wiki/Main_Page"
             html_text = requests.get(url).text
@@ -71,3 +120,37 @@ class WikipediaScraper:
             return data
         except:
             return None
+
+
+if __name__ == "__main__":
+    # test
+    import os
+    import time
+    
+    test_queries = ["dog", "graph theory", "stanford", "computer science", 
+                    "machine learning", "artificial intelligence", "data science",
+                     "data structure", "programming language", "computer", "computer network", 
+                     "computer security", "computer graphics", "computer vision", 
+                     "computer architecture", "computer engineering", "computer hardware",
+                       "computer software", "computer programming", "computer data storage", 
+                       "computer memory", "computer performance", "computer science", "computer scientist",
+                         "computer programming", "computer programmer", "computer keyboard", "computer mouse",
+                           "computer monitor", "computer terminal", "computer network", "computer security", 
+                           "computer graphics", "computer vision", "computer architecture", "computer engineering", 
+                           "computer hardware", "computer software", "computer programming", "computer data storage",
+                             "computer memory", "computer performance", "computer science", "computer scientist", 
+                             "computer programming", "computer programmer", "computer keyboard", "computer mouse", "computer monitor", "computer terminal"]
+
+    for query in test_queries:
+        if os.name == "posix":
+            os.system("clear")
+        if os.name == "nt":
+            os.system("cls")
+
+        wiki = Wiki(query)
+        print(f"{query} : {wiki.define()}")
+        time.sleep(3)
+        # The dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf
+        # In mathematics, graph theory is the study of graphs, which are mathematical structures used to model pairwise relations between objects
+        # Stanford University (officially Leland Stanford Junior University) is a private research university in Stanford, California
+        # . . . 
