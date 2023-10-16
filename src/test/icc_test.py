868ed04127c00b4c52c120bb3d77129b37bec11c
Nikhil Raj <nikhil25803@gmail.com>
2023-10-16 23:14:50 +0530

    Code formatting
    Co-Authored-By: Akash Pambhar <71589813+akashpambhar@users.noreply.github.com>

--- src/test/icc_test.py
+++ src/test/icc_test.py
@@ -1,6 +1,7 @@
 import unittest
 from scrape_up import icc
 
+
 class ICCTest(unittest.TestCase):
     """
     ICC module test.\n
@@ -17,28 +18,57 @@ class ICCTest(unittest.TestCase):
         response = instance.team_rankings("ODI")
         self.assertGreater(len(response), 0, "Team rankings is empty")
         self.assertTrue(isinstance(response, list), "Team rankings is not a list")
-        self.assertTrue(all(isinstance(team, dict) and "rank" in team and "team" in team for team in response), "Incorrect format for team rankings")
+        self.assertTrue(
+            all(
+                isinstance(team, dict) and "rank" in team and "team" in team
+                for team in response
+            ),
+            "Incorrect format for team rankings",
+        )
 
     def test_player_ranking(self):
         instance = icc.ICC()
         response = instance.player_ranking("batting", "TEST")
         self.assertGreater(len(response), 0, "Player ranking is empty")
         self.assertTrue(isinstance(response, list), "Player ranking is not a list")
-        self.assertTrue(all(isinstance(player, dict) and "rank" in player and "name" in player for player in response), "Incorrect format for player rankings")
-    
+        self.assertTrue(
+            all(
+                isinstance(player, dict) and "rank" in player and "name" in player
+                for player in response
+            ),
+            "Incorrect format for player rankings",
+        )
+
     def test_team_rankings_women(self):
         instance = icc.ICC()
         response = instance.team_rankings_women("T20")
         self.assertGreater(len(response), 0, "Team rankings for women is empty")
-        self.assertTrue(isinstance(response, list), "Team rankings for women is not a list")
-        self.assertTrue(all(isinstance(team, dict) and "rank" in team and "team" in team for team in response), "Incorrect format for team rankings for women")
+        self.assertTrue(
+            isinstance(response, list), "Team rankings for women is not a list"
+        )
+        self.assertTrue(
+            all(
+                isinstance(team, dict) and "rank" in team and "team" in team
+                for team in response
+            ),
+            "Incorrect format for team rankings for women",
+        )
 
     def test_player_ranking_women(self):
         instance = icc.ICC()
         response = instance.player_ranking("bowling", "ODI")
         self.assertGreater(len(response), 0, "Player ranking for women is empty")
-        self.assertTrue(isinstance(response, list), "Player ranking for women is not a list")
-        self.assertTrue(all(isinstance(player, dict) and "rank" in player and "name" in player for player in response), "Incorrect format for player rankings for women")
+        self.assertTrue(
+            isinstance(response, list), "Player ranking for women is not a list"
+        )
+        self.assertTrue(
+            all(
+                isinstance(player, dict) and "rank" in player and "name" in player
+                for player in response
+            ),
+            "Incorrect format for player rankings for women",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
