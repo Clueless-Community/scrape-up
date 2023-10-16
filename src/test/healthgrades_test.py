2b937637dc0208ef16d19b6a40a1002ea630a466
AimenYaseen <aimenyaseen6@gmail.com>
2023-10-16 21:35:26 +0500

    test case for get_best_hospitals

--- src/test/healthgrades_test.py
+++ src/test/healthgrades_test.py
@@ -14,6 +14,12 @@ class HealthGradesTest(unittest.TestCase):
     def setUp(self):
         self.instance = healthgrades.HealthGrades()
 
+    def test_get_best_hospitals(self):
+        best_hospitals = self.instance.get_best_hospitals('bihar')
+
+        # assert statements
+        self.assertIsInstance(best_hospitals, list)
+
 
 if __name__ == "__main__":
     unittest.main()
