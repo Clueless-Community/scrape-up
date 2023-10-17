735ab9dfba34f9d5a27a82a4f3e8b33c3cbecd8b
Keshav Vyas <keshavvyas484@gmail.com>
2023-10-17 20:58:40 +0530

    Added assertions

--- src/test/eazydiner_test.py
+++ src/test/eazydiner_test.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from scrape_up.eazydiner import EazyDiner
 
 class EazyDinerTest(unittest.TestCase):
@@ -14,25 +15,39 @@ class EazyDinerTest(unittest.TestCase):
     | `.get_top10()`           | Tests the get_top10() method of the EazyDiner class              |
     """
 
+    def assert_response_keys(self, response, expected_keys):
+        if isinstance(response, str):
+            response_dict = json.loads(response)
+
+            for key in expected_keys:
+                self.assertTrue(
+                    key in response_dict,
+                    f"Key '{key}' is missing in the response."
+                )
+
     def test_get_restaurants(self):
         eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
         restaurants = eazydiner.get_restaurants()
         self.assertIsInstance(restaurants, str)
+        self.assert_response_keys(restaurants, ["restaurants"])
 
     def test_get_breakfast(self):
         eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
         breakfast = eazydiner.get_breakfast()
         self.assertIsInstance(breakfast, str)
+        self.assert_response_keys(breakfast, ["restaurants"])
 
     def test_get_lunch(self):
         eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
         lunch = eazydiner.get_lunch()
         self.assertIsInstance(lunch, str)
+        self.assert_response_keys(lunch, ["restaurants"])
 
     def test_get_dinner(self):
         eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
         dinner = eazydiner.get_dinner()
         self.assertIsInstance(dinner, str)
+        self.assert_response_keys(dinner, ["restaurants"])
 
     def test_dinner_with_discount(self):
         eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
