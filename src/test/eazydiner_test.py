c6ab293af35e742f6c3f28dc614226dfdd568009
Nikhil Raj <nikhil25803@gmail.com>
2023-10-18 19:05:32 +0530

    Code formatting
    Co-Authored-By: Keshav Vyas <69897817+Master-Keshav@users.noreply.github.com>

--- src/test/eazydiner_test.py
+++ src/test/eazydiner_test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 from scrape_up.eazydiner import EazyDiner
 
+
 class EazyDinerTest(unittest.TestCase):
     """
     EazyDiner class test.\n
@@ -21,43 +22,55 @@ class EazyDinerTest(unittest.TestCase):
 
             for key in expected_keys:
                 self.assertTrue(
-                    key in response_dict,
-                    f"Key '{key}' is missing in the response."
+                    key in response_dict, f"Key '{key}' is missing in the response."
                 )
 
     def test_get_restaurants(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         restaurants = eazydiner.get_restaurants()
         self.assertIsInstance(restaurants, str)
         self.assert_response_keys(restaurants, ["restaurants"])
 
     def test_get_breakfast(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         breakfast = eazydiner.get_breakfast()
         self.assertIsInstance(breakfast, str)
         self.assert_response_keys(breakfast, ["restaurants"])
 
     def test_get_lunch(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         lunch = eazydiner.get_lunch()
         self.assertIsInstance(lunch, str)
         self.assert_response_keys(lunch, ["restaurants"])
 
     def test_get_dinner(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         dinner = eazydiner.get_dinner()
         self.assertIsInstance(dinner, str)
         self.assert_response_keys(dinner, ["restaurants"])
 
     def test_dinner_with_discount(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         dinner_discount = eazydiner.dinner_with_discount()
         self.assertIsInstance(dinner_discount, list)
 
     def test_get_top10(self):
-        eazydiner = EazyDiner(location="Delhi NCR")  # Replace with an appropriate location
+        eazydiner = EazyDiner(
+            location="Delhi NCR"
+        )  # Replace with an appropriate location
         top10 = eazydiner.get_top10()
         self.assertIsInstance(top10, dict)
+
 
 if __name__ == "__main__":
     unittest.main()
