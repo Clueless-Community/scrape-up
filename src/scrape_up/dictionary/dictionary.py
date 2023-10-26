ef517684f2b33569ff3445b821a5ac5fcbab6e44
swetha3456 <swetha.murali@iiitb.ac.in>
2023-10-26 19:40:15 +0530

    made requested changes

--- src/scrape_up/dictionary/dictionary.py
+++ src/scrape_up/dictionary/dictionary.py
@@ -2,6 +2,16 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 class Dictionary:
+    """
+    Create an instance of the `Dictionary` class.
+    ```python
+    scraper = IMDB()
+    ```
+    | Methods        | Details                                      |
+    | -------------- | -------------------------------------------- |
+    | `.get_word_of_the_day()` | Returns word of the day from Dictionary.com. |
+    | `.word_of_the_day_definition()` | Returns the definition of the word of the day. |
+    """
     def get_word_of_the_day_url(self):
         with urlopen("https://www.dictionary.com/") as response:
             soup = BeautifulSoup(response, 'html.parser')
@@ -15,9 +25,15 @@ class Dictionary:
         return None
     
     def get_word_of_the_day(self):
+        """
+        Returns a string containing the word of the day.
+        """
         return self.get_word_of_the_day_url().split("/")[-2].split("-")[0]
     
     def word_of_the_day_definition(self):
+        """
+        Returns a string containing the definition of the word of the day.
+        """
         with urlopen(self.get_word_of_the_day_url()) as response:
             soup = BeautifulSoup(response, 'html.parser')
 
@@ -25,14 +41,7 @@ class Dictionary:
                 if para.string and para.string[0] not in "EG":
                     return para.string
 
-        return None
-            
-if __name__ == "__main__":
-    d = Dictionary()
-    print("Word of the day:", d.get_word_of_the_day())
-    print("Definition:", d.word_of_the_day_definition())
-
-            
+        return None           
 
 
 
