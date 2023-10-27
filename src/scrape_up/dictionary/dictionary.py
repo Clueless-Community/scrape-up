72398cacc90524446df002582c064d29e4e28155
swetha3456 <swetha.murali@iiitb.ac.in>
2023-10-27 13:48:12 +0530

    made suggested changes

--- src/scrape_up/dictionary/dictionary.py
+++ src/scrape_up/dictionary/dictionary.py
@@ -5,43 +5,67 @@ class Dictionary:
     """
     Create an instance of the `Dictionary` class.
     ```python
-    scraper = IMDB()
+    scraper = Dictionary()
     ```
     | Methods        | Details                                      |
     | -------------- | -------------------------------------------- |
     | `.get_word_of_the_day()` | Returns word of the day from Dictionary.com. |
     | `.word_of_the_day_definition()` | Returns the definition of the word of the day. |
     """
+
     def get_word_of_the_day_url(self):
-        with urlopen("https://www.dictionary.com/") as response:
-            soup = BeautifulSoup(response, 'html.parser')
+        try:
+            with urlopen("https://www.dictionary.com/") as response:
+                soup = BeautifulSoup(response, 'html.parser')
 
-            for anchor in soup('button'):
-                url = anchor.get('data-linkurl', '/')
+                for anchor in soup('button'):
+                    url = anchor.get('data-linkurl', '/')
 
-                if "word-of-the-day" in url:
-                    return url
-                
-        return None
+                    if "word-of-the-day" in url:
+                        return url
+
+        finally:       
+            return None
     
     def get_word_of_the_day(self):
         """
         Returns a string containing the word of the day.
+
+        ```python
+        scraper = Dictionary()
+        print(scraper.get_word_of_the_day())
+        ```
+
+        Sample output:
+        >> unfalsifiable
         """
-        return self.get_word_of_the_day_url().split("/")[-2].split("-")[0]
+        try:
+            return self.get_word_of_the_day_url().split("/")[-2].split("-")[0]
+        except:
+            return None
     
     def word_of_the_day_definition(self):
         """
         Returns a string containing the definition of the word of the day.
+
+        ```python
+        scraper = Dictionary()
+        print(scraper.word_of_the_day_definition())
+        ```
+
+        Sample output:
+        >> not able to be proven false, and therefore not scientific.
         """
-        with urlopen(self.get_word_of_the_day_url()) as response:
-            soup = BeautifulSoup(response, 'html.parser')
+        try:
+            with urlopen(self.get_word_of_the_day_url()) as response:
+                soup = BeautifulSoup(response, 'html.parser')
 
-            for para in soup('p'):
-                if para.string and para.string[0] not in "EG":
-                    return para.string
+                for para in soup('p'):
+                    if para.string and para.string[0] not in "EG":
+                        return para.string
 
-        return None           
+        finally:
+            return None           
 
 
 
