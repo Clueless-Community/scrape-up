2593059839754e4304eafb085c2188c8e5f06aac
Nikhil Raj <nikhil25803@gmail.com>
2024-05-13 09:03:19 +0530

    Formatting and checks.

--- src/test/bbc_test.py
+++ src/test/bbc_test.py
@@ -29,12 +29,14 @@ class TestBBCNews(unittest.TestCase):
                 self.assertIsInstance(headlines, list)
                 for headline in headlines:
                     self.assertIsInstance(headline, dict)
-                    self.assertIn('index', headline)
-                    self.assertIn('headline', headline)
+                    self.assertIn("index", headline)
+                    self.assertIn("headline", headline)
 
                 # Check if all headlines have unique indices
-                indices = {headline['index'] for headline in headlines}
-                self.assertEqual(len(indices), len(headlines), "Duplicate indices found in headlines")
+                indices = {headline["index"] for headline in headlines}
+                self.assertEqual(
+                    len(indices), len(headlines), "Duplicate indices found in headlines"
+                )
                 # Check if headlines list is not empty
                 self.assertGreater(len(headlines), 0, "No headlines extracted")
         except:
@@ -49,13 +51,19 @@ class TestBBCNews(unittest.TestCase):
             article = self.bbc_scraper.get_article(valid_url)
 
             if article is not None:
-                self.assertIsInstance(article, dict)  # Check if article is a dictionary or not
-                self.assertIn('main_heading', article)  # Does it contain main_heading or not
-                self.assertIn('time', article)  # Does it contain time or not
-                self.assertIn('text', article)  # Does it contain text or not
+                self.assertIsInstance(
+                    article, dict
+                )  # Check if article is a dictionary or not
+                self.assertIn(
+                    "main_heading", article
+                )  # Does it contain main_heading or not
+                self.assertIn("time", article)  # Does it contain time or not
+                self.assertIn("text", article)  # Does it contain text or not
 
             invalid_url = "https://www.bbc.co.uk/news/non-existent-article"  # Test with an invalid article URL
-            invalid_article = self.bbc_scraper.get_article(invalid_url)  # Should return None
+            invalid_article = self.bbc_scraper.get_article(
+                invalid_url
+            )  # Should return None
             self.assertIsNone(invalid_article, "Invalid URL should return None")
         except:
             return None
