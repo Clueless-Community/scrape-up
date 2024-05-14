171c9a0e7c32493556933e8588ea61b0caa30ea5
Chaitany Arora <arorachaitany15@gmail.com>
2024-05-14 11:08:26 +0530

    [FEATURE] Add Leetcode #876

--- src/scrape_up/leetcode/leetcode_scraper.py
+++ src/scrape_up/leetcode/leetcode_scraper.py
@@ -146,7 +146,7 @@ class LeetCode:
             total_problems = soup.find(
                 "div",
                 {
-                    "class": "text-[24px] font-medium text-label-1 dark:text-dark-label-1"
+                    "class": "text-[30px] font-semibold leading-[32px]"
                 },
             )
             return {
