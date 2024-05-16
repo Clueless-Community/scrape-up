f5e9fe5e9451ac37781be390e5a1c71bac5efd89
Nikhil Raj <nikhil25803@gmail.com>
2024-05-16 17:20:56 +0530

    Made some changes

--- src/scrape_up/flexjobs/flexjobs.py
+++ src/scrape_up/flexjobs/flexjobs.py
@@ -4,17 +4,8 @@ from bs4 import BeautifulSoup
 
 class FlexJobs:
     """
-    Create an instance of `FlexJobs` class with the name of the flex_jobs.
-
     ```python
-    # Create a FlexJobs instance
     flex_jobs = FlexJobs(search_query, location_query, min_jobs)
-    # flex_jobs = FlexJobs(search_query)
-    # flex_jobs = FlexJobs(search_query, location_query)
-    # flex_jobs = FlexJobs(search_query, min_jobs=10)
-
-    # Get job listings
-    jobs = flex_jobs.get_jobs()
     ```
 
     Attributes:
@@ -31,7 +22,9 @@ class FlexJobs:
     | `.scrape_job_info(job_listing)` | Extracts job details from a given job listing HTML element               |
     """
 
-    def __init__(self, search_query: str, location_query: str = '', min_jobs: int = 100):
+    def __init__(
+        self, search_query: str, location_query: str = "", min_jobs: int = 100
+    ):
         self.search_query = search_query
         self.location_query = location_query
         self.min_jobs = min_jobs
@@ -61,13 +54,13 @@ class FlexJobs:
         ```
         """
         # Formatting search and location queries for URL
-        search = self.search_query.strip().replace(' ', '%20')
-        location = self.location_query.strip().replace(' ', '%2C%20')
+        search = self.search_query.strip().replace(" ", "%20")
+        location = self.location_query.strip().replace(" ", "%2C%20")
 
         # Constructing base URL
-        base_url: str = f'https://www.flexjobs.com/search?searchkeyword={search}'
-        if location != '':
-            base_url = f'{base_url}&joblocations={location}'
+        base_url: str = f"https://www.flexjobs.com/search?searchkeyword={search}"
+        if location != "":
+            base_url = f"{base_url}&joblocations={location}"
 
         job_listings = []
         page = 1
@@ -77,8 +70,8 @@ class FlexJobs:
             response = requests.get(url)
             if response.status_code == 200:
                 # Parsing HTML content
-                soup = BeautifulSoup(response.content, 'html.parser')
-                job_cards = soup.find_all('div', class_='sc-jv5lm6-0')
+                soup = BeautifulSoup(response.content, "html.parser")
+                job_cards = soup.find_all("div", class_="sc-jv5lm6-0")
                 if not job_cards:
                     break
                 # Extracting job information from each listing
@@ -116,30 +109,46 @@ class FlexJobs:
         ```
         """
         # Extracting job details from HTML elements
-        job_title_element = job_listing.find('a', id=lambda x: x and x.startswith('job-name-'))
+        job_title_element = job_listing.find(
+            "a", id=lambda x: x and x.startswith("job-name-")
+        )
         job_title = job_title_element.text.strip() if job_title_element else None
 
-        link: str = 'https://www.flexjobs.com/' + job_title_element['href'] if job_title_element else None
+        link: str = (
+            "https://www.flexjobs.com/" + job_title_element["href"]
+            if job_title_element
+            else None
+        )
 
-        location_element = job_listing.find('span', id=lambda x: x and x.startswith('allowedlocation-'))
+        location_element = job_listing.find(
+            "span", id=lambda x: x and x.startswith("allowedlocation-")
+        )
         location = location_element.text.strip() if location_element else None
 
-        job_age_element = job_listing.find('div', id=lambda x: x and x.startswith('job-age-'))
+        job_age_element = job_listing.find(
+            "div", id=lambda x: x and x.startswith("job-age-")
+        )
         job_age = job_age_element.text.strip() if job_age_element else None
 
-        remote_option = job_listing.find('li', id=lambda x: x and x.startswith('remoteoption-'))
+        remote_option = job_listing.find(
+            "li", id=lambda x: x and x.startswith("remoteoption-")
+        )
         remote = remote_option.text.strip() if remote_option else None
 
-        job_schedule = job_listing.find('li', id=lambda x: x and x.startswith('jobschedule-'))
+        job_schedule = job_listing.find(
+            "li", id=lambda x: x and x.startswith("jobschedule-")
+        )
         schedule = job_schedule.text.strip() if job_schedule else None
 
-        job_type = job_listing.find('li', id=lambda x: x and x.startswith('jobTypes-'))
+        job_type = job_listing.find("li", id=lambda x: x and x.startswith("jobTypes-"))
         job_type_text = job_type.text.strip() if job_type else None
 
-        salary_range = job_listing.find('li', id=lambda x: x and x.startswith('salartRange-'))
+        salary_range = job_listing.find(
+            "li", id=lambda x: x and x.startswith("salartRange-")
+        )
         salary = salary_range.text.strip() if salary_range else None
 
-        description = job_listing.find('p', class_='sc-jv5lm6-4')
+        description = job_listing.find("p", class_="sc-jv5lm6-4")
         job_description = description.text.strip() if description else None
 
         job = {
@@ -151,7 +160,7 @@ class FlexJobs:
             "schedule": schedule,
             "job_type": job_type_text,
             "salary": salary,
-            "description": job_description
+            "description": job_description,
         }
 
         return job
--- src/test/flexjobs_test.py
+++ src/test/flexjobs_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.scrape_up.flexjobs import FlexJobs
+from scrape_up.flexjobs import FlexJobs
 
 
 class TestFlexJobs(unittest.TestCase):
@@ -17,8 +17,10 @@ class TestFlexJobs(unittest.TestCase):
     def test_get_jobs_with_min_jobs_limit(self):
         flexjobs = FlexJobs("python developer", min_jobs=5)
         jobs = flexjobs.get_jobs()
-        self.assertTrue(len(jobs) >= 5, "Number of jobs retrieved exceeds max jobs limit")
+        self.assertTrue(
+            len(jobs) >= 5, "Number of jobs retrieved exceeds max jobs limit"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
