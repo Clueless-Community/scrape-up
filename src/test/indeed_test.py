dbf3e6bc27a0c8c1e5e2c1bfb46d1fc6459efc3e
AKASH RAI <akashrai4546@gmail.com>
2024-05-16 18:57:32 +0530

    feat: Add Indeed Scrapper - Improved Test cases

--- src/test/indeed_test.py
+++ src/test/indeed_test.py
@@ -10,7 +10,7 @@ class TestIndeed(unittest.TestCase):
         self.assertTrue(len(jobs) > 0, "No jobs found for valid search query")
 
     def test_get_jobs_with_location_query(self):
-        indeed = Indeed("java", "India")
+        indeed = Indeed("java", "Mumbai")
         jobs = indeed.get_jobs()
         self.assertTrue(len(jobs) > 0, "No jobs found for valid location query")
 
