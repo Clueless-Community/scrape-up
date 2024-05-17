94b84c9ef12473c99f97b973a73684ec23a6183d
Agastya Kumar Yadav <agastya3636@outlook.com>
2024-05-17 10:31:26 +0000

    updated atcoder.py

--- src/scrape_up/atcoder/atcoder.py
+++ src/scrape_up/atcoder/atcoder.py
@@ -4,13 +4,20 @@ from bs4 import BeautifulSoup
 
 class Atcoder:
     '''
+    atc=Atcoder(user="chokudai")
+    stc.get_profile()
+
+    | Methods           | Details                                                                            |
+    | ----------------- | ---------------------------------------------------------------------------------- |
+    | `.get_profile()`  | Returns the user data in json format.                                              |
+
     '''
     def __init__(self,user):
         self.user=user
     
     def get_profile(self):
         try:
-            url = "https://atcoder.jp/users/chokudai"
+            url = f"https://atcoder.jp/users/{self.user}"
             headers = {"User-Agent": "scrapeup"}
             response = requests.get(url, headers=headers)
             soup = BeautifulSoup(response.text, "html.parser")
@@ -19,22 +26,22 @@ class Atcoder:
 
             row=table[0].find_all('tr')
             for r in row :
-                # print(r)
+               
                 user_details[r.find('th').text.replace(' ','_')]=r.find('td').text.replace('\n',' ').strip()
 
             row=table[1].find_all('tr')
             for r in row :
-                # print(r)
+               
                 user_details["Algorithm_"+r.find('th').text.replace(' ','_')]=r.find('td').text.replace('\n',' ').strip()
 
-            url="https://atcoder.jp/users/chokudai?contestType=heuristic"
+            url=f"https://atcoder.jp/users/{self.user}?contestType=heuristic"
             response = requests.get(url, headers=headers)
             soup = BeautifulSoup(response.text, "html.parser")
-            table=soup.find_all('table',class_="dl-table");
+            table=soup.find_all('table',class_="dl-table")
             row=table[1].find_all('tr')
             for r in row :
-                # print(r)
+                
                 user_details["Heuristic_"+r.find('th').text.replace(' ','_')]=r.find('td').text.replace('\n',' ').strip()
-            return json.dumps(user_data)
+            return json.dumps(user_details)
         except:
             return None
