6dfc86da964319520706ef894bc4b5c23efeed45
Nikhil Raj <nikhil25803@gmail.com>
2024-05-24 15:42:04 +0530

    Check

--- src/test/atcoder_test.py
+++ src/test/atcoder_test.py
@@ -1,14 +1,14 @@
 import unittest
 import json
-from src.scrape_up.atcoder import Atcoder
+from scrape_up.atcoder import Atcoder
 
 
 class TestAtcoder(unittest.TestCase):
     """
-        | Methods           | Details                                                                            |
-        | ----------------- | ---------------------------------------------------------------------------------- |
-        | `.get_profile()`  | Returns the user data in json format.                                              |
-        | `get_contests()`  | Returns future_contests , past_contests , skill_tests etc in json format.          |
+    | Methods           | Details                                                                            |
+    | ----------------- | ---------------------------------------------------------------------------------- |
+    | `.get_profile()`  | Returns the user data in json format.                                              |
+    | `get_contests()`  | Returns future_contests , past_contests , skill_tests etc in json format.          |
 
     """
 
@@ -30,15 +30,28 @@ class TestAtcoder(unittest.TestCase):
             profile_data = json.loads(profile)
 
             # Check if profile_data is a dictionary
-            self.assertIsInstance(profile_data, dict, "Profile data should be a dictionary")
+            self.assertIsInstance(
+                profile_data, dict, "Profile data should be a dictionary"
+            )
 
             # Check if certain keys exist in the profile_data
             expected_keys = [
-                "Country/Region", "Birth_Year", "Twitter_ID", "TopCoder_ID",
-                "Codeforces_ID", "Affiliation", "Algorithm_Rank", "Algorithm_Rating",
-                "Algorithm_Highest_Rating", "Algorithm_Rated_Matches_", "Algorithm_Last_Competed",
-                "Heuristic_Rank", "Heuristic_Rating", "Heuristic_Highest_Rating",
-                "Heuristic_Rated_Matches_", "Heuristic_Last_Competed"
+                "Country/Region",
+                "Birth_Year",
+                "Twitter_ID",
+                "TopCoder_ID",
+                "Codeforces_ID",
+                "Affiliation",
+                "Algorithm_Rank",
+                "Algorithm_Rating",
+                "Algorithm_Highest_Rating",
+                "Algorithm_Rated_Matches_",
+                "Algorithm_Last_Competed",
+                "Heuristic_Rank",
+                "Heuristic_Rating",
+                "Heuristic_Highest_Rating",
+                "Heuristic_Rated_Matches_",
+                "Heuristic_Last_Competed",
             ]
 
             for key in expected_keys:
@@ -56,7 +69,9 @@ class TestAtcoder(unittest.TestCase):
             self.assertIsNotNone(contests, "Failed to fetch contests")
 
             # Check if contests is a dictionary
-            self.assertIsInstance(contests, dict, "Contests data should be a dictionary")
+            self.assertIsInstance(
+                contests, dict, "Contests data should be a dictionary"
+            )
 
             # Check if certain keys exist in the contests data
             expected_keys = ["active", "Upcoming", "Recent", "Permanent"]
@@ -65,8 +80,9 @@ class TestAtcoder(unittest.TestCase):
                 self.assertIn(key, contests, f"Missing expected key: {key}")
 
                 # Check if the value for each key is a dictionary
-                self.assertIsInstance(contests[key], dict, f"The value for '{key}' should be a dictionary")
-
+                self.assertIsInstance(
+                    contests[key], dict, f"The value for '{key}' should be a dictionary"
+                )
 
         except:
             return None
