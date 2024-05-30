8e5e44885c260eebbbd1a6a096f0dbc7f1e07673
Nikhil Raj <nikhil25803@gmail.com>
2024-05-30 19:46:30 +0530

    Check

--- src/scrape_up/zomato/zomato.py
+++ src/scrape_up/zomato/zomato.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup as bs
-
-from src.scrape_up.config.request_config import RequestConfig, get
+from scrape_up.config.request_config import RequestConfig, get
 
 
 class Zomato:
@@ -57,7 +56,7 @@ class Zomato:
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win 64 ; x64) Apple WeKit /537.36(KHTML , like Gecko) Chrome/80.0.3987.162 Safari/537.36"
             }
-            #self.config = config
+            # self.config = config
             if self.config.headers == {}:
                 self.config.set_headers(headers)
             response = get(page_url, self.config).text
@@ -103,4 +102,3 @@ class Zomato:
             return restaurant_data
         except:
             return None
-        
--- src/test/zomato_test.py
+++ src/test/zomato_test.py
@@ -1,5 +1,6 @@
 import unittest
-from src.scrape_up.zomato import Zomato
+from scrape_up.zomato import Zomato
+
 
 class TestZomato(unittest.TestCase):
     """
@@ -7,45 +8,77 @@ class TestZomato(unittest.TestCase):
     | ----------------------------- | -------------------------------------------------------------------------- |
     | `.get_restaurants_details(page_url)`    | Returns the restraunt data with name, cuisine, area, rating, offers, etc|
     """
-    
+
     def setUp(self):
         self.restaurant = Zomato()
-        
+
     def test_get_restaurants_details(self):
-        result=self.restaurant.get_restaurants_details(page_url="https://www.zomato.com/ncr/khatirdari-ek-riwaz-paras-tierea-noida/order")
+        result = self.restaurant.get_restaurants_details(
+            page_url="https://www.zomato.com/ncr/khatirdari-ek-riwaz-paras-tierea-noida/order"
+        )
         try:
-            self.assertIsNotNone(result,"Failed to fetch restaurant details. Please provide a url of type https://www.zomato.com/ncr/music-mountains-hillside-cafe-greater-kailash-gk-1-new-delhi/order")
-            self.assertIsInstance(result,dict,"Restaurant details should be a dictionary object")
+            self.assertIsNotNone(
+                result,
+                "Failed to fetch restaurant details. Please provide a url of type https://www.zomato.com/ncr/music-mountains-hillside-cafe-greater-kailash-gk-1-new-delhi/order",
+            )
+            self.assertIsInstance(
+                result, dict, "Restaurant details should be a dictionary object"
+            )
             try:
                 if result is not None:
                     for key in result:
-                        self.assertIn(result[key],f"no key named {key}")
+                        self.assertIn(result[key], f"no key named {key}")
             except:
                 return None
-            
+
             try:
                 if result["name"] is not None:
-                    self.assertIsInstance(result["name"],str,"name should be a string")
+                    self.assertIsInstance(
+                        result["name"], str, "name should be a string"
+                    )
                 if result["cuisine"] is not None:
-                    self.assertIsInstance(result["cuisine"],list),"cuisine should be a list"
+                    (
+                        self.assertIsInstance(result["cuisine"], list),
+                        "cuisine should be a list",
+                    )
                 if result["area"] is not None:
-                    self.assertIsInstance(result["area"],str,"area should be a string")
+                    self.assertIsInstance(
+                        result["area"], str, "area should be a string"
+                    )
                 if result["dining_rating"] is not None:
-                    self.assertIsInstance(result["dining_rating"],str,"dining rating should be a string representing a number")
-                if result["dining_review_count"] is not None:    
-                    self.assertIsInstance(result["dining_review_count"],str,"dining review count should be a string representing a number")
+                    self.assertIsInstance(
+                        result["dining_rating"],
+                        str,
+                        "dining rating should be a string representing a number",
+                    )
+                if result["dining_review_count"] is not None:
+                    self.assertIsInstance(
+                        result["dining_review_count"],
+                        str,
+                        "dining review count should be a string representing a number",
+                    )
                 if result["delivery_rating"] is not None:
-                    self.assertIsInstance(result["delivery_rating"],str,"delivery rating should be a string representing a number")
-                if result["delivery_review_count"] is not None:    
-                    self.assertIsInstance(result["delivery_review_count"],str,"delivery review count should be a string representing a number")    
+                    self.assertIsInstance(
+                        result["delivery_rating"],
+                        str,
+                        "delivery rating should be a string representing a number",
+                    )
+                if result["delivery_review_count"] is not None:
+                    self.assertIsInstance(
+                        result["delivery_review_count"],
+                        str,
+                        "delivery review count should be a string representing a number",
+                    )
                 if result["offers"] is not None:
-                    self.assertIsInstance(result["offers"],list,"offers should be a list")   
+                    self.assertIsInstance(
+                        result["offers"], list, "offers should be a list"
+                    )
             except:
                 return None
-  
-        except:  
+
+        except:
             return None
-        
+
+
 if __name__ == "__main__":
     unittest.main()
-            
