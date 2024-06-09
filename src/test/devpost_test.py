662ef8b2d81aa3a4a44cb1bffb6e110750dc7688
Lingamuneni Santhosh Siddhardha <103999924+Santhosh-Siddhardha@users.noreply.github.com>
2024-06-09 22:37:30 +0530

    Update devpost_test.py
    Added Comments

--- src/test/devpost_test.py
+++ src/test/devpost_test.py
@@ -3,6 +3,15 @@ from src.scrape_up.devpost import Devpost
 
 
 class TestDevpost(unittest.TestCase):
+    """
+    | Methods            | Details                                                                                                              |
+    | ------------------ | -------------------------------------------------------------------------------------------------------------------- |
+    | `.get_projects()` | Returns the latest projects along with their decription, like and commment count, image and member details. |
+    | `.search(topic)` | Returns the searched projects along with their decription, like and commment count, image and member details. |
+    | `.get_hackathons()` | Returns the latest hackathons along with their title, participants, prizes, deadlines.          |
+    | `.get_featured()` | Returns the latest featured projects along with their decription, like and commment count, image and member details. |
+    | `.get_winner()` | Returns the latest winning projects along with their decription, like and commment count, image and member details. |
+    """
 
     def setUp(self):
         """
